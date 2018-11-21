7f3ac9b1fa06c77103aeb6da4be091b98edd1cc6
Ivan Mladenov <madritistaaa@gmail.com>
2018-11-21 08:27:49 +0200

    Task 02 (#4)
    * Add n square solution
    
    * Add readme with task
    
    * Change init of result array

---
+++ 02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Task02
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var input = Console.ReadLine()
+				.Split(' ')
+				.Select(int.Parse)
+				.ToArray();
+
+			var result = new int[input.Length];
+
+			for (int i = 0; i < result.Length; i++)
+			{
+				result[i] = 1;
+
+				for (int j = 0; j < result.Length; j++)
+				{
+					if (j != i)
+					{
+						result[i] *= input[j];
+					}
+				}
+			}
+
+			Console.WriteLine(string.Join(' ', result));
+		}
+	}
+}
