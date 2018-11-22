88f33a7e4b1d86540910883a7c895739e777cbb7
Ivan Mladenov <madritistaaa@gmail.com>
2018-11-22 08:56:52 +0200

    Task 03 (#5)
    * Add description
    
    * Add source of solution
    
    * Add solution

---
+++ Task03/Node.cs
@@ -0,0 +1,18 @@
+﻿namespace Task03
+{
+	public class Node
+	{
+		public Node(string value = null, Node left = null, Node right = null)
+		{
+			this.Value = value;
+			this.Left = left;
+			this.Right = right;
+		}
+
+		public string Value { get; set; }
+
+		public Node Left { get; set; }
+
+		public Node Right { get; set; }
+	}
+}
