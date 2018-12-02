dfe3c66c2068afc890797d356afecb766b60d9b7
Ivan Mladenov <madritistaaa@gmail.com>
2018-12-02 11:05:57 +0200

    Problem 12 (#20)
    * Add task description
    
    * Implement solution with bfs

---
+++ Problem12/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem12
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var n = int.Parse(Console.ReadLine());
+
+			var x = Console.ReadLine()
+				.Split(' ')
+				.Select(int.Parse)
+				.ToArray();
+
+			var waysToClimb = Climb(0, n, x);
+
+			Console.WriteLine(waysToClimb);
+		}
+
+		public static int Climb(int climbedStairs, int n, int[] x)
+		{
+			if (climbedStairs > n)
+			{
+				return 0;
+			}
+
+			if (climbedStairs == n)
+			{
+				return 1;
+			}
+
+			var result = 0;
+
+			for (int i = 0; i < x.Length; i++)
+			{
+				var nextClimb = climbedStairs + x[i];
+
+				result += Climb(nextClimb, n, x);
+			}
+
+			return result;
+		}
+	}
+}
