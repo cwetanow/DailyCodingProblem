562726de1958b046cff58325ebb049d4af21c5c6
Ivan Mladenov <madritistaaa@gmail.com>
2018-12-24 14:03:42 +0200

    Tests (#32)
    * Add test project
    
    * Write tests for problem 01
    
    * Add test command to travis

---
+++ Tests/Problem01Tests/SolutionTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Tests.Problem01Tests
+{
+	[TestFixture]
+	public class SolutionTests
+	{
+		[Test]
+		public void TestProblem01Solution_NumbersAddUpToK_ShouldReturnTrue()
+		{
+			// Arrange
+			var numbers = new[] { 10, 15, 3, 7 }
+				.ToList();
+			var k = 17;
+
+			// Act
+			var result = Problem01.Program.TwoNumbersEqual(numbers, k);
+
+			// Assert
+			Assert.IsTrue(result);
+		}
+
+		[Test]
+		public void TestProblem01Solution_NumbersDoNotAddUpToK_ShouldReturnFalse()
+		{
+			// Arrange
+			var numbers = new[] { 10, 15, 3, 7 }
+				.ToList();
+			var k = 16;
+
+			// Act
+			var result = Problem01.Program.TwoNumbersEqual(numbers, k);
+
+			// Assert
+			Assert.IsFalse(result);
+		}
+	}
+}
