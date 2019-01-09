168cfa71982f87dc4cdd498bcec36227b4433926
Ivan Mladenov <madritistaaa@gmail.com>
2019-01-09 14:07:28 +0200

    Problem 50 (#33)
    * Add readme
    
    * Add node
    
    * Implement solution
    
    * Write tests

---
+++ Problem50/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Problem50
+{
+	public class Program
+	{
+		private const char Add = '+';
+		private const char Subtract = '-';
+		private const char Multiply = '*';
+		private const char Divide = '/';
+
+		public static void Main()
+		{
+			var root = new Node('*', new Node('+', new Node(3), new Node(2)), new Node('+', new Node(4), new Node(5)));
+
+			var result = Evaluate(root);
+
+			System.Console.WriteLine(result);
+		}
+
+		public static int Evaluate(Node node)
+		{
+			if (node.Value.HasValue)
+			{
+				return node.Value.Value;
+			}
+
+			var left = Evaluate(node.Left);
+			var right = Evaluate(node.Right);
+
+			var result = Evaluate(left, right, node.Sign).Value;
+
+			return result;
+		}
+
+		private static int? Evaluate(int left, int right, char sign)
+		{
+			if (sign == Add)
+			{
+				return left + right;
+			}
+
+			if (sign == Subtract)
+			{
+				return left - right;
+			}
+
+			if (sign == Multiply)
+			{
+				return left * right;
+			}
+
+			if (sign == Divide)
+			{
+				return left / right;
+			}
+
+			return null;
+		}
+	}
+
+	public class Node
+	{
+		public Node(int? value)
+		{
+			Value = value;
+		}
+
+		public Node(char sign)
+		{
+			Sign = sign;
+		}
+
+		public Node(char sign, Node left, Node right)
+			: this(sign)
+		{
+			Left = left;
+			Right = right;
+		}
+
+		public int? Value { get; set; }
+
+		public char Sign { get; set; }
+
+		public Node Left { get; set; }
+
+		public Node Right { get; set; }
+	}
+}
