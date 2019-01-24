2980eed7b6d9f8a9bd91a3e69b990033f5ac078d
Ivan Mladenov <madritistaaa@gmail.com>
2019-01-24 13:34:39 +0200

    Problem 62 (#35)
    * Add description
    
    * Write tests
    
    * Add solution

---
+++ Tests/Problem62Tests/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Problem62;
+
+namespace Tests.Problem62Tests
+{
+	[TestFixture]
+	public class SolutionTests
+	{
+		[TestCase(2, 2, 2)]
+		[TestCase(5, 5, 70)]
+		public void TestProblem62Solution_ShouldReturnCorrectResult(int n, int m, int expectedResult)
+		{
+			// Arrange
+			// Act
+			var result = Program.GetNumberOfWaysToCorner(n, m);
+
+			// Assert
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
