9f030fb42d9a9fd925b7390e29a51ca20ec15483
Ivan Mladenov <madritistaaa@gmail.com>
2019-01-24 12:09:50 +0200

    Problem 63 (#34)
    * Add description
    
    * Write tests
    
    * Write solution
    
    * Add more tests

---
+++ Tests/Problem63Tests/SolutionTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Problem63;
+
+namespace Tests.Problem63Tests
+{
+	[TestFixture]
+	public class SolutionTests
+	{
+		[TestCase("MASS")]
+		[TestCase("NOB")]
+		[TestCase("OBQ")]
+		[TestCase("BNA")]
+		[TestCase("CQO")]
+		[TestCase("FOAM")]
+		public void TestProblem63Solution_WordExists_ShouldReturnTrue(string word)
+		{
+			// Arrange
+			var matrix = new char[][]{
+				new char[]{'F', 'A', 'C', 'I'},
+				new char[]{'O', 'B', 'Q', 'P'},
+				new char[]{'A', 'N', 'O', 'B'},
+				new char[]{ 'M', 'A', 'S', 'S' }
+			};
+
+			// Act
+			var result = Program.CanWordBeFound(matrix, word);
+
+			// Assert
+			Assert.IsTrue(result);
+		}
+
+		[TestCase("TEST")]
+		[TestCase("NOTEXISTINGWORD")]
+		public void TestProblem63Solution_WordDoesNotExist_ShouldReturnFalse(string word)
+		{
+			// Arrange
+			var matrix = new char[][]{
+				new char[]{'F', 'A', 'C', 'I'},
+				new char[]{'O', 'B', 'Q', 'P'},
+				new char[]{'A', 'N', 'O', 'B'},
+				new char[]{ 'M', 'A', 'S', 'S' }
+			};
+
+			// Act
+			var result = Program.CanWordBeFound(matrix, word);
+
+			// Assert
+			Assert.IsFalse(result);
+		}
+	}
+}
