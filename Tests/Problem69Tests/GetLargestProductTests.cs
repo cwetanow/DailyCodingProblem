4ba3afb47ded127d6a05fd7c5cec9644fab41e2a
Ivan Mladenov <madritistaaa@gmail.com>
2019-01-29 09:59:05 +0200

    Problem 69 (#37)
    * Add description
    
    * Write test
    
    * Implement solution

---
+++ Tests/Problem69Tests/GetLargestProductTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Tests.Problem69Tests
+{
+	[TestFixture]
+	public class GetLargestProductTests
+	{
+		[Test]
+		public void TestGetLargestProduct_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var numbers = new int[] { -10, -10, 5, 2 };
+
+			var expectedResult = 500;
+
+			// Act
+			var result = Problem69.Program.GetLargestProduct(numbers);
+
+			// Assert
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
