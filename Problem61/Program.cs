314d12d176742e4f3309c784e271262ffbc68255
Ivan Mladenov <madritistaaa@gmail.com>
2019-01-29 13:38:27 +0200

    Problem 61 (#38)
    * Add task description
    
    * Write tests
    
    * Implement solution

---
+++ Problem61/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Problem61
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		}
+
+		public static long Pow(int number, int power)
+		{
+			var pows = new int[power];
+			pows[0] = 1;
+			pows[1] = number;
+
+			var currentPow = 1;
+			var currentNumber = number;
+
+			while (true)
+			{
+				if (currentPow == power)
+				{
+					return currentNumber;
+				}
+
+
+				if (currentPow * 2 < power)
+				{
+					currentNumber *= currentNumber;
+					currentPow *= 2;
+				}
+				else
+				{
+					currentNumber *= number;
+					currentPow++;
+				}
+			}
+		}
+	}
+}
