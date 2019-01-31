2553255671379cecbbebc7a9d97644897a163fb9
Ivan Mladenov <madritistaaa@gmail.com>
2019-01-31 09:06:01 +0200

    Problem 26 (#39)
    * Add description
    
    * Implement solution
    
    * Make root and count protected
    
    * Extend linked list to have contains
    
    * Write add tests
    
    * Write tests for remove

---
+++ Tests/Problem26Tests/RemoveKthLastElementTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Tests.Problem26Tests.Mock;
+
+namespace Tests.Problem26Tests
+{
+	[TestFixture]
+	public class RemoveKthLastElementTests
+	{
+		[Test]
+		public void TestRemoveKthLastElement_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var list = new ExtendedLinkedList<int>();
+
+			list.Add(4);
+			list.Add(3);
+			list.Add(2);
+			list.Add(1);
+
+			var k = 2;
+			var expected = 2;
+
+			// Act
+			var actual = list.RemoveKthLastElement(k);
+
+			// Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void TestRemoveKthLastElement_ShouldRemoveElement()
+		{
+			// Arrange
+			var list = new ExtendedLinkedList<int>();
+
+			list.Add(4);
+			list.Add(3);
+			list.Add(2);
+			list.Add(1);
+
+			var k = 2;
+			var element = 2;
+
+			// Act
+			list.RemoveKthLastElement(k);
+
+			// Assert
+			Assert.IsFalse(list.Contains(element));
+		}
+	}
+}
