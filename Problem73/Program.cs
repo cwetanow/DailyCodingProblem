2df9548ccd68b46465e3aed70760297bf18594eb
Ivan Mladenov <madritistaaa@gmail.com>
2019-02-05 10:25:35 +0200

    Problem 73 (#43)
    * Add task
    
    * Implement linked list
    
    * Write tests
    
    * Reverse list

---
+++ Problem73/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Problem73
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ Problem73/README.md
@@ -0,0 +1,3 @@
+﻿This problem was asked by Google.
+
+Given the head of a singly linked list, reverse it in-place.
