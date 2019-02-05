003fa10007a835b038a9d33af2f12f9dc04ab0c3
Ivan Mladenov <madritistaaa@gmail.com>
2019-02-05 09:01:51 +0200

    Problem 74 (#41)
    * Add task
    
    * Add solution skeleton
    
    * Write tests
    
    * Write more tests
    
    * Implement naive solution:
    
    * Optimise solution
    
    * Optimise solution to be O(n)

---
+++ Problem74/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Problem74
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		}
+
+		public static int GetXOccurencesCount(int n, int x)
+		{
+			var count = 0;
+
+			for (int i = 1; i <= n; i++)
+			{
+				if (x % i == 0 && x / i <= n)
+				{
+					count++;
+				}
+			}
+
+			return count;
+		}
+	}
+}
---
+++ Tests/Problem74Tests/GetXOccurencesCountTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Tests.Problem74Tests
+{
+	[TestFixture]
+	public class GetXOccurencesCountTests
+	{
+		[TestCase(6, 12, 4)]
+		[TestCase(2, 12, 0)]
+		public void TestGetXOccurencesCount_ShouldReturnCorrectly(int n, int x, int expected)
+		{
+			// Arrange, Act
+			var actual = Problem74.Program.GetXOccurencesCount(n, x);
+
+			// Assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
