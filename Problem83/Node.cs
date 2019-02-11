1d49c4852d73cb52d3f07f38c51b4e618c7d8a0d
Ivan Mladenov <madritistaaa@gmail.com>
2019-02-11 18:15:19 +0200

    Problem 83 (#47)
    * Add readme
    
    * Add node class
    
    * Reverse binary tree

---
+++ Problem83/Node.cs
@@ -0,0 +1,23 @@
+﻿namespace Problem83
+{
+		public class Node<T>
+		{
+			public Node(T value)
+			{
+				Value = value;
+			}
+
+			public Node(T value, Node<T> left, Node<T> right)
+				: this(value)
+			{
+				this.Left = left;
+				this.Right = right;
+			}
+
+			public Node<T> Right { get; set; }
+
+			public Node<T> Left { get; set; }
+
+			public T Value { get; set; }
+		}
+}
