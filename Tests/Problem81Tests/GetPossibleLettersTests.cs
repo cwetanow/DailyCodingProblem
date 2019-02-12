4873ed4c3a10bcbe55e9d4670a4b92f9bcc83758
Ivan Mladenov <madritistaaa@gmail.com>
2019-02-12 08:57:23 +0200

    Problem 81 (#49)
    * Add task
    
    * Add skeleton for solution
    
    * Write tests
    
    * Implement solution

---
+++ Tests/Problem81Tests/GetPossibleLettersTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Tests.Problem81Tests
+{
+	[TestFixture]
+	public class GetPossibleLettersTests
+	{
+		[Test]
+		public void TestGetPossibleLetters_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var mappings = new Dictionary<char, string[]> {
+				{ '2', new string[] { "a", "b", "c" } },
+				{ '3', new string[] { "d", "e", "f" } }
+			};
+
+			var inputNumber = "23";
+
+			var expectedResult = new string[] {
+				"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"
+			};
+
+			// Act
+			var actualResult = Problem81.Program.GetPossibleLetters(inputNumber, mappings);
+
+			// Assert
+			CollectionAssert.AreEqual(expectedResult, actualResult);
+		}
+	}
+}
