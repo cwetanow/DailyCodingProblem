8522fab515f6c81d067d9347d73672d64a4eaf14
Ivan Mladenov <madritistaaa@gmail.com>
2019-02-14 13:51:02 +0200

    Problem 80 (#51)
    * Add task
    
    * Copy node implementation
    
    * Write tests
    
    * Implement solution

---
+++ Problem80/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Problem80
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		public static T GetDeepestNode<T>(Node<T> root)
+		{
+			var (value, level) = GetDeepestNode(root, 0);
+
+			return value;
+		}
+
+		private static (T value, int level) GetDeepestNode<T>(Node<T> node, int currentLevel)
+		{
+			if (node.Left == null && node.Right == null)
+			{
+				return (node.Value, currentLevel);
+			}
+
+			var deepestValue = default(T);
+			var deepestLevel = currentLevel;
+
+			if (node.Left != null)
+			{
+				(deepestValue, deepestLevel) = GetDeepestNode<T>(node.Left, currentLevel + 1);
+			}
+
+			if (node.Right != null)
+			{
+				var (value, level) = GetDeepestNode<T>(node.Right, currentLevel + 1);
+
+				if (level > deepestLevel)
+				{
+					deepestLevel = level;
+					deepestValue = value;
+				}
+			}
+
+			return (deepestValue, deepestLevel);
+		}
+	}
+}
---
+++ Problem80/README.md
@@ -0,0 +1,9 @@
+﻿This problem was asked by Google.
+
+Given the root of a binary tree, return a deepest node. For example, in the following tree, return d.
+
+    a
+   / \
+  b   c
+ /
+d
