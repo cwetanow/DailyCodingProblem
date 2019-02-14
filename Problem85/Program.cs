f6d6c20a80121947c953e249d929a823be5f7f95
Ivan Mladenov <madritistaaa@gmail.com>
2019-02-14 11:23:58 +0200

    Problem 85 (#50)
    * Add readme
    
    * Scaffold solution
    
    * Write tests
    
    * Implement solution

---
+++ Problem85/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Problem85
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+		}
+
+		public static int GetNumber(int b, int x, int y)
+		{
+			return b / 1 == 1 ? x : y;
+		}
+	}
+}
