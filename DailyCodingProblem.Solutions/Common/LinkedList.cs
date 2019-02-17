c989a2a3eaef915d2b192250d28b33f362f71343
Ivan <madritistaaa@gmail.com>
2019-02-17 10:44:41 +0200

    Add linked list implementation

---
+++ DailyCodingProblem.Solutions/Common/LinkedList.cs
@@ -0,0 +1,50 @@
+﻿namespace DailyCodingProblem.Solutions.Common
+{
+	public class LinkedList<T>
+	{
+		protected class LinkedListNode
+		{
+			public LinkedListNode(T value)
+			{
+				Value = value;
+			}
+
+			public LinkedListNode(T value, LinkedListNode previous)
+				: this(value)
+			{
+				previous.Next = this;
+			}
+
+			public T Value { get; set; }
+
+			public LinkedListNode Next { get; set; }
+		}
+
+		protected LinkedListNode root;
+		protected int count;
+
+		public void Add(T element)
+		{
+			if (this.root == null)
+			{
+				this.count = 0;
+				this.root = new LinkedListNode(element);
+			}
+			else
+			{
+				var currentNode = this.root;
+				var previousNode = (LinkedListNode)null;
+
+				while (currentNode != null)
+				{
+					previousNode = currentNode;
+					currentNode = currentNode.Next;
+				}
+
+				currentNode = new LinkedListNode(element, previousNode);
+			}
+
+			this.count++;
+		}
+	}
+}
