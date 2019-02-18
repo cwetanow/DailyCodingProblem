6d32fa0dc4543eef5cceb786632e4bc2d489c5e1
Ivan <madritistaaa@gmail.com>
2019-02-18 18:17:27 +0200

    Add tasks from today

---
+++ DailyCodingProblem.Solutions/Problem89/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using DailyCodingProblem.Solutions.Common;
+
+namespace DailyCodingProblem.Solutions.Problem89
+{
+	public class Solution
+	{
+		public static bool IsBinaryTree<T>(Node<T> root)
+			where T : IComparable<T>
+		{
+			if (root.Left == null && root.Right == null)
+			{
+				return true;
+			}
+
+			if (root.Left != null)
+			{
+				if (root.Left.Value.CompareTo(root.Value) >= 0)
+				{
+					return false;
+				}
+
+				if (!IsBinaryTree(root.Left))
+				{
+					return false;
+				}
+			}
+
+			if (root.Right != null)
+			{
+				if (root.Right.Value.CompareTo(root.Value) <= 0)
+				{
+					return false;
+				}
+
+				if (!IsBinaryTree(root.Right))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
