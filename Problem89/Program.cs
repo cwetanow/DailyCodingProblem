10b2e7c7297588b4485b77b60de3cca49bc2b476
Ivan Mladenov <madritistaaa@gmail.com>
2019-02-18 08:56:20 +0200

    Problem 89 (#52)
    * Add readme
    
    * Write tests
    
    * Implement solution

---
+++ Problem89/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Problem89
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public static bool IsBinaryTree<T>(Node<T> root)
+			where T : IComparable<T>
+		{
+			if (root.Left == null && root.Right == null)
+			{
+				return true;
+			}
+
+			if (root.Left != null)
+			{
+				if (root.Left.Value.CompareTo(root.Value) >= 0)
+				{
+					return false;
+				}
+
+				if (!IsBinaryTree(root.Left))
+				{
+					return false;
+				}
+			}
+
+			if (root.Right != null)
+			{
+				if (root.Right.Value.CompareTo(root.Value) <= 0)
+				{
+					return false;
+				}
+
+				if (!IsBinaryTree(root.Right))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
