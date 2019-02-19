a0228ab315721e6c161b11bedce21b6c7d572a2f
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-02-19 08:33:20 +0200

    Start implementing solution

---
+++ DailyCodingProblem.Solutions/Problem17/Solution.cs
@@ -0,0 +1,35 @@
+﻿using DailyCodingProblem.Solutions.Common;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem17
+{
+	public class Solution
+	{
+		public static (string longestPath, int length) GetLongestPath(string input)
+		{
+			if (string.IsNullOrEmpty(input))
+			{
+				return (string.Empty, 0);
+			}
+
+			var root = ParseInput(input);
+
+			return GetLongestPath(root, root.Value, root.Value.Length);
+		}
+
+		private static Node<string> ParseInput(string input)
+		{
+
+		}
+
+		private static (string longestPath, int length) GetLongestPath(Node<string> node, string currentLongestPath, int currentLength)
+		{
+			if (!node.Children.Any())
+			{
+				return (currentLongestPath, currentLength);
+			}
+
+			var longestPath = string.Empty;
+		}
+	}
+}
