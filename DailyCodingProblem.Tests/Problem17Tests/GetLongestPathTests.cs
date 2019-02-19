91549596ac0302137531049271519b15c808c456
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-02-19 08:21:50 +0200

    Writet tests

---
+++ DailyCodingProblem.Tests/Problem17Tests/GetLongestPathTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Tests.Problem17Tests
+{
+	[TestFixture]
+	public class GetLongestPathTests
+	{
+		[TestCase(@"dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext", @"dir/subdir2/subsubdir2/file2.ext")]
+		public void TestGetLongestPath_ShouldReturnCorrectLongestPath(string path, string expectedLongestPath)
+		{
+			// Arrange, Act
+			var (longestPath, _) = Solutions.Problem17.Solution.GetLongestPath(path);
+
+			// Assert
+			Assert.AreEqual(expectedLongestPath, longestPath);
+		}
+
+		[TestCase(@"dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext", 32)]
+		public void TestGetLongestPath_ShouldReturnCorrectLongestPathLength(string path, string expectedLength)
+		{
+			// Arrange, Act
+			var (_, length) = Solutions.Problem17.Solution.GetLongestPath(path);
+
+			// Assert
+			Assert.AreEqual(expectedLength, length);
+		}
+	}
+}
