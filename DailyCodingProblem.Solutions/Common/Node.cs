9f21ed5f4ebf7591896a24392bbe044d7d1276a9
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-02-19 08:14:22 +0200

    Add tree node

---
+++ DailyCodingProblem.Solutions/Common/Node.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace DailyCodingProblem.Solutions.Common
+{
+	public class Node<T>
+	{
+		public Node(T value)
+		{
+			this.Value = value;
+			this.Children = new List<Node<T>>();
+		}
+
+		public Node(T value, Node<T> parent = null, ICollection<Node<T>> children = null)
+			: this(value)
+		{
+			this.Parent = parent;
+
+			if (children != null)
+			{
+				this.Children = children;
+			}
+		}
+
+		public T Value { get; set; }
+
+		public ICollection<Node<T>> Children { get; private set; }
+
+		public Node<T> Parent { get; set; }
+	}
+}
