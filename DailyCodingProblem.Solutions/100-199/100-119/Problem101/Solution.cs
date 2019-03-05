677325bcd0bd3c373515f6bd9338a8e8d54f27f0
Ivan Mladenov <madritistaaa@gmail.com>
2019-03-05 11:17:44 +0200

    Problem 101 (#57)
    * Add readme
    
    * Write tests
    
    * Implement solution

---
+++ DailyCodingProblem.Solutions/100-199/100-119/Problem101/Solution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem101
+{
+	public class Solution
+	{
+		public static (int left, int right) GetPrimeNumbersWithSum(int sum)
+		{
+			var left = 2;
+			var right = sum - left;
+
+			var solutions = new List<(int left, int right)>();
+
+			while (left <= right)
+			{
+				if (IsPrime(left) && IsPrime(right))
+				{
+					solutions.Add((left, right));
+				}
+
+				left++;
+				right--;
+			}
+
+			var solution = solutions.FirstOrDefault();
+
+			for (var i = 1; i < solutions.Count; i++)
+			{
+				solution = CompareSolutions(solution, solutions[i]);
+			}
+
+			return solution;
+		}
+
+		private static (int left, int right) CompareSolutions((int left, int right) first,
+			(int left, int right) second)
+		{
+			if (first.left <= second.left && first.right < second.right)
+			{
+				return first;
+			}
+
+			return second;
+		}
+
+		private static bool IsPrime(int number)
+		{
+			var sqrt = Math.Sqrt(number);
+
+			for (var i = 2; i < sqrt; i++)
+			{
+				if (number % i == 0)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ DailyCodingProblem.Tests/100-199/100-119/Problem101Tests/GetPrimeNumbersWithCurrentSumTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Tests.Problem101Tests
+{
+	[TestFixture]
+	public class GetPrimeNumbersWithCurrentSumTests
+	{
+		[TestCase(4, 2, 2)]
+		public void TestGetPrimeNumbersWithCurrentSum_ShouldReturnCorrectly(int input, int expectedLeft, int expectedRight)
+		{
+			// Arrange, Act
+			var (left, right) = Solutions.Problem101.Solution.GetPrimeNumbersWithSum(input);
+
+			// Assert
+			Assert.AreEqual(expectedLeft, left);
+			Assert.AreEqual(expectedRight, right);
+		}
+	}
+}
