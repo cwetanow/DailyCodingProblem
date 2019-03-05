1ae83922fc6b4b392b42abd4a6c2091f465680e0
Ivan Mladenov <madritistaaa@gmail.com>
2019-03-05 11:00:56 +0200

    Problem 103 (#56)
    * Add task description
    
    * Write tests
    
    * Implement solution

---
+++ DailyCodingProblem.Solutions/100-199/100-119/Problem103/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem103
+{
+	public class Solution
+	{
+		public static string GetShortestSubstringContainingAllCharacters(string input, char[] characters)
+		{
+			if (!characters.All(input.Contains))
+			{
+				return null;
+			}
+
+			var withoutLeft = GetShortestSubstringContainingAllCharacters(input.Substring(1, input.Length - 1), characters);
+
+			var withoutRight = GetShortestSubstringContainingAllCharacters(input.Substring(0, input.Length - 1), characters);
+
+			if (withoutLeft == null && withoutRight == null)
+			{
+				return input;
+			}
+
+			if (withoutLeft == null)
+			{
+				return withoutRight;
+			}
+
+			if (withoutRight == null)
+			{
+				return withoutLeft;
+			}
+
+			return withoutRight.Length < withoutLeft.Length ? withoutRight : withoutLeft;
+		}
+	}
+}
---
+++ DailyCodingProblem.Tests/100-199/100-119/Problem103Tests/GetShortestSubstringContainingAllCharactersTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Tests.Problem103Tests
+{
+	[TestFixture]
+	public class GetShortestSubstringContainingAllCharactersTests
+	{
+		[Test]
+		public void TestGetShortestSubstringContainingAllCharacters_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = "figehaeci";
+			var characters = new char[] { 'a', 'e', 'i' };
+			var expectedResult = "aeci";
+
+			// Act
+
+			var shortestSubstring = Solutions.Problem103.Solution.GetShortestSubstringContainingAllCharacters(input, characters);
+
+			// Assert
+			Assert.AreEqual(expectedResult, shortestSubstring);
+		}
+
+		[Test]
+		public void TestGetShortestSubstringContainingAllCharacters_ThereIsNoSubstring_ShouldReturnNull()
+		{
+			// Arrange
+			var input = "test";
+			var characters = new char[] { 'a' };
+
+			// Act
+
+			var shortestSubstring = Solutions.Problem103.Solution.GetShortestSubstringContainingAllCharacters(input, characters);
+
+			// Assert
+			Assert.IsNull(shortestSubstring);
+		}
+	}
+}
