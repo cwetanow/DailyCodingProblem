98ee55ec651d84b44792349baf81db4fbd8d5caf
Ivan Mladenov <madritistaaa@gmail.com>
2019-03-06 10:30:09 +0200

    Problem 106 (#58)
    * Add task
    
    * Implement solution
    
    * Write tests

---
+++ DailyCodingProblem.Solutions/100-199/100-119/Problem106/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace DailyCodingProblem.Solutions.Problem106
+{
+	public class Solution
+	{
+		public static bool CanReachLastIndex(int[] list)
+		{
+			var index = 0;
+
+			while (index < list.Length)
+			{
+				var current = list[index];
+
+				index += current;
+
+				if (index == list.Length - 1)
+				{
+					return true;
+				}
+
+				if (current == 0 && index < list.Length - 1)
+				{
+					return false;
+				}
+			}
+
+			return false;
+		}
+	}
+}
---
+++ DailyCodingProblem.Tests/100-199/100-119/Problem106Tests/CanReachLastIndexTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Tests.Problem106Tests
+{
+	[TestFixture]
+	public class CanReachLastIndexTests
+	{
+		[Test]
+		public void TestCanReachLastIndex_CanReach_ShouldReturnTrue()
+		{
+			// Arrange
+			var list = new[] { 2, 0, 1, 0 };
+
+			// Act
+			var result = Solutions.Problem106.Solution.CanReachLastIndex(list);
+
+			// Assert
+			Assert.IsTrue(result);
+		}
+
+		[Test]
+		public void TestCanReachLastIndex_CanNotReach_ShouldReturnFalse()
+		{
+			// Arrange
+			var list = new[] { 1, 1, 0, 1 };
+
+			// Act
+			var result = Solutions.Problem106.Solution.CanReachLastIndex(list);
+
+			// Assert
+			Assert.IsFalse(result);
+		}
+	}
+}
