9b719b8f712a146dfd44901f63adee8edda77f54
Ivan Mladenov <madritistaaa@gmail.com>
2019-03-06 10:45:08 +0200

    Problem 102 (#59)
    * Add readme
    
    * Implement solution
    
    * Write tests

---
+++ DailyCodingProblem.Solutions/100-199/100-119/Problem102/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem102
+{
+	public class Solution
+	{
+		public static int[] GetContiguousElementsThatSumToK(int[] list, int k)
+		{
+			var result = new int[] { };
+
+			for (var i = 0; i < list.Length; i++)
+			{
+				var currentSum = 0;
+
+				for (var j = i; j < list.Length; j++)
+				{
+					currentSum += list[j];
+
+					if (currentSum > k)
+					{
+						break;
+					}
+
+					if (currentSum == k)
+					{
+						result = list
+							.Skip(i)
+							.Take(j - i + 1)
+							.ToArray();
+
+						return result;
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ DailyCodingProblem.Tests/100-199/100-119/Problem102Tests/GetContiguousElementsThatSumToKTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Tests.Problem102Tests
+{
+	[TestFixture]
+	public class GetContiguousElementsThatSumToKTests
+	{
+		[Test]
+		public void TestGetContiguousElementsThatSumToK_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var k = 9;
+			var list = new[] { 1, 2, 3, 4, 5 };
+
+			var expectedResult = new[] { 2, 3, 4 };
+
+			// Act
+			var actual = Solutions.Problem102.Solution.GetContiguousElementsThatSumToK(list, k);
+
+			// Assert
+			CollectionAssert.AreEqual(expectedResult, actual);
+		}
+	}
+}
