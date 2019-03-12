b281c5bbca3e7670ca6eea0ff36941ebf3ea880b
Ivan Mladenov <madritistaaa@gmail.com>
2019-03-12 18:35:50 +0200

    Problem 113 (#61)
    * Add task
    
    * Write tests
    
    * Implement solution

---
+++ DailyCodingProblem.Solutions/100-199/100-119/Problem113/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem113
+{
+	public class Solution
+	{
+		public static string ReverseWords(string input)
+		{
+			return string.Join(' ', input
+				.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+				.Reverse()
+				.ToArray());
+		}
+	}
+}
---
+++ DailyCodingProblem.Tests/100-199/100-119/Problem113Tests/ReverseWordsTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Tests.Problem113Tests
+{
+	[TestFixture]
+	public class ReverseWordsTests
+	{
+		[Test]
+		public void TestReverseWords_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = "hello world here";
+			var expectedResult = "here world hello";
+
+			// Act
+			var actual = Solutions.Problem113.Solution.ReverseWords(input);
+
+			// Assert
+			Assert.AreEqual(expectedResult, actual);
+		}
+	}
+}
