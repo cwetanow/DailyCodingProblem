553c65438a05290207a62f9e6529f6e09081c11a
Ivan Mladenov <madritistaaa@gmail.com>
2019-03-19 15:52:07 +0200

    Problem 118 (#63)
    * Add task description
    
    * Write tests
    
    * Implement solution

---
+++ DailyCodingProblem.Solutions/100-199/100-119/Problem118/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem118
+{
+	public class Solution
+	{
+		public static int[] GetSquaredSortedElements(int[] input)
+		{
+			return input
+				.Select(el => (int)Math.Pow(el, 2))
+				.OrderBy(el => el)
+				.ToArray();
+		}
+	}
+}
---
+++ DailyCodingProblem.Tests/100-199/100-119/Problem118Tests/GetSquaredSortedElementsTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Tests.Problem118Tests
+{
+	[TestFixture]
+	public class GetSquaredSortedElementsTests
+	{
+		[Test]
+		public void TestGetSquaredSortedElements_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new int[] { -9, -2, 0, 2, 3 };
+
+			var expectedResult = new int[] { 0, 4, 4, 9, 81 };
+
+			// Act
+			var actualResult = DailyCodingProblem.Solutions.Problem118.Solution.GetSquaredSortedElements(input);
+
+			// Assert
+			CollectionAssert.AreEqual(expectedResult, actualResult);
+		}
+	}
+}
