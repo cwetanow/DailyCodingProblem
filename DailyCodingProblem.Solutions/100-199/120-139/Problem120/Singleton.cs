90051627e84d1e3d2e023bdc79e67711ca78c29a
Ivan Mladenov <madritistaaa@gmail.com>
2019-03-20 11:16:37 +0200

    Problem 120 (#65)
    * Add task description
    
    * Implement solution
    
    * Write tests

---
+++ DailyCodingProblem.Solutions/100-199/120-139/Problem120/Singleton.cs
@@ -0,0 +1,35 @@
+﻿namespace DailyCodingProblem.Solutions.Problem120
+{
+	public class Singleton
+	{
+		private static Singleton FirstInstance;
+		private static Singleton SecondInstance;
+
+		private static int Counter = 1;
+
+		protected Singleton(int id)
+		{
+			Id = id;
+		}
+
+		public static void Reset()
+		{
+			FirstInstance = null;
+			SecondInstance = null;
+			Counter = 1;
+		}
+
+		public static void InitializeSingletons(int evenId, int oddId)
+		{
+			FirstInstance = new Singleton(evenId);
+			SecondInstance = new Singleton(oddId);
+		}
+
+		public static Singleton GetInstance()
+		{
+			return Counter++ % 2 == 0 ? FirstInstance : SecondInstance;
+		}
+
+		public int Id { get; set; }
+	}
+}
---
+++ DailyCodingProblem.Tests/100-199/120-139/Problem120Tests/SingletonTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Tests.Problem120Tests
+{
+	[TestFixture]
+	public class SingletonTests
+	{
+		[TestCase(1, 2)]
+		[TestCase(5, 20)]
+		public void TestSingleton_OnOddCall_ShouldReturnCorrectSingleton(int evenId, int oddId)
+		{
+			// Arrange
+			Solutions.Problem120.Singleton.Reset();
+
+			Solutions.Problem120.Singleton.InitializeSingletons(evenId, oddId);
+
+			// Act
+			var instanceId = Solutions.Problem120.Singleton.GetInstance().Id;
+
+			// Assert
+			Assert.AreEqual(oddId, instanceId);
+		}
+
+		[TestCase(1, 2)]
+		[TestCase(5, 20)]
+		public void TestSingleton_OnEvenCall_ShouldReturnCorrectSingleton(int evenId, int oddId)
+		{
+			// Arrange
+			Solutions.Problem120.Singleton.Reset();
+
+			Solutions.Problem120.Singleton.InitializeSingletons(evenId, oddId);
+			Solutions.Problem120.Singleton.GetInstance();
+
+			// Act
+			var instanceId = Solutions.Problem120.Singleton.GetInstance().Id;
+
+			// Assert
+			Assert.AreEqual(evenId, instanceId);
+		}
+	}
+}
