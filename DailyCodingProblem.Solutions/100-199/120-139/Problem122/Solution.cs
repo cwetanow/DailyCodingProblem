91e164b80661a1db41141b7e3ae82e940df30de0
Ivan Mladenov <madritistaaa@gmail.com>
2019-03-22 17:28:04 +0200

    Problem 122 (#66)
    * Add task description
    
    * Add solution skeleton
    
    * Write tests
    
    * Implement solution

---
+++ DailyCodingProblem.Solutions/100-199/120-139/Problem122/Solution.cs
@@ -0,0 +1,45 @@
+﻿namespace DailyCodingProblem.Solutions.Problem122
+{
+	public class Solution
+	{
+		private static int MaxNumberOfCoins = 0;
+
+		private static int Rows;
+		private static int Cols;
+
+		public static int GetMaxNumberOfCoins(int[][] matrix)
+		{
+			Rows = matrix.Length;
+			Cols = matrix[0].Length;
+
+			GetMaxNumberOfCoins(matrix, 0, 0, 0);
+
+			return MaxNumberOfCoins;
+		}
+
+		private static void GetMaxNumberOfCoins(int[][] matrix, int row, int col, int current)
+		{
+			current = current + matrix[row][col];
+
+			if (row == Rows - 1 && col == Cols - 1)
+			{
+				if (MaxNumberOfCoins < current)
+				{
+					MaxNumberOfCoins = current;
+				}
+
+				return;
+			}
+
+			if (row < Rows - 1)
+			{
+				GetMaxNumberOfCoins(matrix, row + 1, col, current);
+			}
+
+			if (col < Cols - 1)
+			{
+				GetMaxNumberOfCoins(matrix, row, col + 1, current);
+			}
+		}
+	}
+}
---
+++ DailyCodingProblem.Tests/100-199/120-139/Problem122Tests/GetMaxNumberOfCoinsTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Tests.Problem122Tests
+{
+	[TestFixture]
+	public class GetMaxNumberOfCoinsTests
+	{
+		[Test]
+		public void TestGetMaxNumberOfCoins_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new int[3][]
+			{
+				new int[]{0,3,1,1},
+				new int[]{2,0,0,4},
+				new int[]{1,5,3,1},
+			};
+
+			var expectedResult = 12;
+
+			// Act
+			var result = Solutions.Problem122.Solution.GetMaxNumberOfCoins(input);
+
+			// Assert
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
