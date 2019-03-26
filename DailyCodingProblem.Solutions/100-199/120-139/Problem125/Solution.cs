2f47836adc102fb0c75f4240d65bf6ec2c9fd478
Ivan Mladenov <madritistaaa@gmail.com>
2019-03-26 10:29:33 +0200

    Problem 125 (#68)
    * Add task
    
    * Implement solution
    
    * Write tests

---
+++ DailyCodingProblem.Solutions/100-199/120-139/Problem125/Solution.cs
@@ -0,0 +1,29 @@
+﻿using DailyCodingProblem.Solutions.Common;
+using System.Collections.Generic;
+
+namespace DailyCodingProblem.Solutions.Problem125
+{
+	public class Solution
+	{
+		private static readonly Dictionary<int, Node<int>> nodes = new Dictionary<int, Node<int>>();
+
+		public static int[] GetNodesWithSumK(Node<int> node, int k)
+		{
+			if (node == null)
+			{
+				return null;
+			}
+
+			var remaining = k - node.Value;
+
+			if (nodes.ContainsKey(remaining))
+			{
+				return new int[] { nodes[remaining].Value, node.Value };
+			}
+
+			nodes.Add(node.Value, node);
+
+			return GetNodesWithSumK(node.Left, k) ?? GetNodesWithSumK(node.Right, k);
+		}
+	}
+}
---
+++ DailyCodingProblem.Tests/100-199/120-139/Problem125Tests/GetNodesWithSumKTests.cs
@@ -0,0 +1,30 @@
+﻿using DailyCodingProblem.Solutions.Common;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Tests.Problem125Tests
+{
+	[TestFixture]
+	public class GetNodesWithSumKTests
+	{
+		[Test]
+		public void TestGetNodesWithSumK_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var root = new Node<int>(10,
+				new Node<int>(5),
+				new Node<int>(15,
+					new Node<int>(11),
+					new Node<int>(15)));
+
+			var k = 20;
+
+			var expectedResult = new int[] { 5, 15 };
+
+			// Act
+			var result = Solutions.Problem125.Solution.GetNodesWithSumK(root, k);
+
+			// Assert
+			CollectionAssert.AreEquivalent(expectedResult, result);
+		}
+	}
+}
