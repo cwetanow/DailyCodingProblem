d5a465cae18e00e8094ae66c7a6dd65873982ce6
Ivan Mladenov <madritistaaa@gmail.com>
2019-03-26 13:34:05 +0200

    Problem 123 (#69)
    * Add task
    
    * Implement solution
    
    * Write tests

---
+++ DailyCodingProblem.Solutions/100-199/120-139/Problem123/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace DailyCodingProblem.Solutions.Problem123
+{
+	public class Solution
+	{
+		public static bool IsNumber(string input)
+		{
+			return double.TryParse(input, NumberStyles.Any, CultureInfo.InvariantCulture, out double result);
+		}
+	}
+}
---
+++ DailyCodingProblem.Tests/100-199/120-139/Problem123Tests/IsNumberTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Tests.Problem123Tests
+{
+	[TestFixture]
+	public class IsNumberTests
+	{
+		[TestCase("10")]
+		[TestCase("-10")]
+		[TestCase("10.1")]
+		[TestCase("-10.1")]
+		[TestCase("1e5")]
+		public void TestIsNumber_IsNumber_ShouldReturnTrue(string input)
+		{
+			// Arrange, Act
+			var result = Solutions.Problem123.Solution.IsNumber(input);
+
+			// Assert
+			Assert.IsTrue(result);
+		}
+
+		[TestCase("a")]
+		[TestCase("x 1")]
+		[TestCase("a -2")]
+		[TestCase("-")]
+		public void TestIsNumber_IsNotNumber_ShouldReturnFalse(string input)
+		{
+			// Arrange, Act
+			var result = Solutions.Problem123.Solution.IsNumber(input);
+
+			// Assert
+			Assert.IsFalse(result);
+		}
+	}
+}
