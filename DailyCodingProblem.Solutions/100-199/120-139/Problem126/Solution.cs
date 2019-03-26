5b7e1714fcacf6ef98f3abe858d7beab7c50d730
Ivan Mladenov <madritistaaa@gmail.com>
2019-03-26 10:21:12 +0200

    Problem 126 (#67)
    * Add readme
    
    * Write tests
    
    * Implement solution

---
+++ DailyCodingProblem.Solutions/100-199/120-139/Problem126/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem126
+{
+	public class Solution
+	{
+		public static IList<int> RotateList(IList<int> list, int k)
+		{
+			for (int i = 0; i < k; i++)
+			{
+				var element = list.First();
+
+				list.RemoveAt(0);
+
+				list.Add(element);
+			}
+
+			return list;
+		}
+	}
+}
---
+++ DailyCodingProblem.Tests/100-199/120-139/Problem126Tests/RotateListTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DailyCodingProblem.Tests.Problem126Tests
+{
+	[TestFixture]
+	public class RotateListTests
+	{
+		[Test]
+		public void TestRotateList_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new List<int> { 1, 2, 3, 4, 5, 6 };
+			var k = 2;
+
+			var expectedResult = new List<int> { 3, 4, 5, 6, 1, 2 };
+
+			// Act
+			var actual = Solutions.Problem126.Solution.RotateList(input, k);
+
+			// Assert
+			CollectionAssert.AreEqual(expectedResult, actual);
+		}
+	}
+}
