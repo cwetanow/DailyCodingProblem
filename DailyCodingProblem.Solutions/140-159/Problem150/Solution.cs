ad39054d613ff6bc8d345ada8a9a0e1664c760fb
Ivan Mladenov <madritistaaa@gmail.com>
2019-04-30 20:09:00 +0300

    Problem 150 (#76)
    * Add task
    
    * Implement solution
    
    * Write test

---
+++ DailyCodingProblem.Solutions/140-159/Problem150/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem150
+{
+	public class Solution
+	{
+		public static IEnumerable<(int x, int y)> FindNearestKPoints(IEnumerable<(int x, int y)> points,
+			(int x, int y) center, int k)
+		{
+			return points
+				.OrderBy(p => GetDistance(p, center))
+				.Take(k)
+				.ToList();
+		}
+
+		private static double GetDistance((int x, int y) point, (int x, int y) otherPoint)
+		{
+			var distance = Math.Pow(point.x - otherPoint.x, 2) + Math.Pow(point.y - otherPoint.y, 2);
+
+			distance = Math.Sqrt(distance);
+
+			return distance;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/140-159/Problem150/Tests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem150
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestFindNearestKPoints_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var points = new List<(int x, int y)>
+			{
+				(0, 0), (5, 4), (3, 1)
+			};
+
+			var center = (1, 2);
+			var k = 2;
+
+			var expectedResult = new List<(int x, int y)>
+			{
+				(0,0),(3,1)
+			};
+
+			// Act
+			var result = Solution.FindNearestKPoints(points, center, k);
+
+			// Assert
+			CollectionAssert.AreEqual(expectedResult, result);
+		}
+	}
+}
