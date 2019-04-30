7a6534d74483c5c2fa457e0eca42d3782aae271b
Ivan Mladenov <madritistaaa@gmail.com>
2019-04-30 19:53:39 +0300

    Problem 155 (#75)
    * Add readme
    
    * Implement solution
    
    * Write tests

---
+++ DailyCodingProblem.Solutions/140-159/Problem155/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem155
+{
+	public class Solution
+	{
+		public static int FindMajorityElement(int[] numbers)
+		{
+			return numbers
+				.GroupBy(n => n)
+				.OrderByDescending(g => g.Count())
+				.First()
+				.Key;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/140-159/Problem155/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem155
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestFindMajorityElement_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var numbers = new int[] { 1, 2, 1, 1, 3, 4, 0 };
+			var expectedResult = 1;
+
+			// Act
+			var actual = Solution.FindMajorityElement(numbers);
+
+			// Assert
+			Assert.AreEqual(expectedResult, actual);
+		}
+	}
+}
