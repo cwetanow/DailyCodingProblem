4a569aecfa5b9d98b6667f500ea847ad7da654f3
Ivan Mladenov <madritistaaa@gmail.com>
2019-04-30 15:26:35 +0300

    Problem 161 (#74)
    * Add readme
    
    * Impement solution
    
    * Write tests

---
+++ DailyCodingProblem.Solutions/100-199/160-179/Problem161/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem161
+{
+	public class Solution
+	{
+		public static string GetReversedBitRepresentation(uint number)
+		{
+			return string.Join(string.Empty, Convert.ToString(number, 2)
+				.Reverse());
+		}
+	}
+}
---
+++ DailyCodingProblem.Tests/100-199/160-179/Problem161Tests/GetReversedBitRepresentationTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Tests.Problem161Tests
+{
+	[TestFixture]
+	public class GetReversedBitRepresentationTests
+	{
+		[Test]
+		public void TestGetReversedBitRepresentation_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var number = 4042322160;// 1111 0000 1111 0000 1111 0000 1111 0000;
+
+			var expectedResult = "00001111000011110000111100001111";
+
+			// Act
+			var result = Solutions.Problem161.Solution.GetReversedBitRepresentation(number);
+
+			// Assert
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
