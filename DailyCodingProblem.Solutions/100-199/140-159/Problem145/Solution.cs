683a8b589f34feb8cd90d9e26b1dd5681e85228a
Ivan Mladenov <madritistaaa@gmail.com>
2019-05-01 22:12:44 +0300

    Problem 145 (#78)
    * Add solution
    
    * Fix folder structure

---
+++ DailyCodingProblem.Solutions/100-199/140-159/Problem145/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace DailyCodingProblem.Solutions.Problem145
+{
+	public class Solution
+	{
+		public static Common.LinkedList<int> SwapEveryTwoNodes(Common.LinkedList<int> list)
+		{
+			var current = list.Root;
+
+			while (current != null)
+			{
+				SwapValues(current, current.Next);
+
+				current = current.Next.Next;
+			}
+
+			return list;
+		}
+
+		private static void SwapValues(Common.LinkedList<int>.LinkedListNode node, Common.LinkedList<int>.LinkedListNode otherNode)
+		{
+			var swap = node.Value;
+			node.Value = otherNode.Value;
+			otherNode.Value = swap;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/140-159/Problem145/Tests.cs
@@ -0,0 +1,32 @@
+﻿using DailyCodingProblem.Solutions.Common;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem145
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestSwapEveryTwoNodes_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var list = new LinkedList<int>();
+			list.Add(1);
+			list.Add(2);
+			list.Add(3);
+			list.Add(4);
+
+			var expectedList = new LinkedList<int>();
+			expectedList.Add(2);
+			expectedList.Add(1);
+			expectedList.Add(4);
+			expectedList.Add(3);
+
+			// Act
+			var result = Solution.SwapEveryTwoNodes(list);
+
+			// Assert
+			Assert.AreEqual(expectedList, result);
+		}
+	}
+}
