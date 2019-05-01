9fd91b0e24891177a94068416211653f989d081d
Ivan Mladenov <madritistaaa@gmail.com>
2019-05-01 22:09:26 +0300

    Problem 163 (#77)
    * Add description
    
    * Write tests
    
    * Implement solution

---
+++ DailyCodingProblem.Solutions/100-199/160-179/Problem163/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace DailyCodingProblem.Solutions.Problem163
+{
+	public class Solution
+	{
+		public static int EvaluateReversePolishNotation(object[] items)
+		{
+			var stack = new Stack<int>();
+
+			foreach (var item in items)
+			{
+				if (item is int number)
+				{
+					stack.Push(number);
+				}
+
+				if (item is char @operator)
+				{
+					var b = stack.Pop();
+					var a = stack.Pop();
+
+					var result = DoOperation(a, b, @operator);
+
+					stack.Push(result);
+				}
+			}
+
+			return stack.Pop();
+		}
+
+		private static int DoOperation(int a, int b, char @operator)
+		{
+			if (@operator == '+')
+			{
+				return a + b;
+			}
+
+			if (@operator == '-')
+			{
+				return a - b;
+			}
+
+			if (@operator == '*')
+			{
+				return a * b;
+			}
+
+			if (@operator == '/')
+			{
+				return a / b;
+			}
+
+			return 0;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/160-179/Problem163/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem163
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase(8, new object[] { 5, 3, '+' })]
+		[TestCase(5, new object[] { 15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-' })]
+		public void TestEvaluateReversePolishNotation_ShouldReturnCorrectly(int expectedResult, object[] items)
+		{
+			// Arrange, Act
+			var result = Solution.EvaluateReversePolishNotation(items);
+
+			// Assert
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
