88dd4050304dbef09c0744e52c991573648b5190
Ivan Mladenov <madritistaaa@gmail.com>
2019-05-03 13:47:30 +0300

    Problem 164 (#80)
    * Add task
    
    * Solve task
    
    * Write tests

---
+++ DailyCodingProblem.Solutions/100-199/160-179/Problem164/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace DailyCodingProblem.Solutions.Problem164
+{
+	public class Solution
+	{
+		public static int FindDuplicate(int[] array)
+		{
+			var set = new HashSet<int>();
+
+			foreach (var number in array)
+			{
+				if (set.Contains(number))
+				{
+					return number;
+				}
+
+				set.Add(number);
+			}
+
+			return 0;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/160-179/Problem164/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem164
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestFindDuplicate_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new[] { 1, 2, 3, 4, 4 };
+			var expectedResult = 4;
+
+			// Act
+			var result = Solution.FindDuplicate(input);
+
+			// Assert
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
