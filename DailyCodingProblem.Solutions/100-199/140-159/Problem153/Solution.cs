1190a95980f01fea07b0448b8a253aad68c516fe
Ivan Mladenov <madritistaaa@gmail.com>
2019-05-03 19:05:39 +0300

    Problem 153 (#81)
    * Add task description
    
    * Write tests
    
    * Implement brute force solution

---
+++ DailyCodingProblem.Solutions/100-199/140-159/Problem153/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem153
+{
+	public class Solution
+	{
+		// This solution assumes both words exist in the text
+		public static int FindSmallestDistanceBetweenWords(string text, string word, string otherWord)
+		{
+			var splitText = text.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+				.ToList();
+
+			var smallestDistance = splitText.Count - 1;
+
+			var firstWordIndexes = new List<int>();
+			var otherWordIndexes = new List<int>();
+
+			for (var i = 0; i < splitText.Count; i++)
+			{
+				if (splitText[i] == word)
+				{
+					firstWordIndexes.Add(i);
+				}
+				else if (splitText[i] == otherWord)
+				{
+					otherWordIndexes.Add(i);
+				}
+			}
+
+			foreach (var firstWordIndex in firstWordIndexes)
+			{
+				foreach (var otherWordIndex in otherWordIndexes)
+				{
+					var distance = Math.Abs(firstWordIndex - otherWordIndex) - 1;
+
+					if (smallestDistance > distance)
+					{
+						smallestDistance = distance;
+					}
+				}
+			}
+
+			return smallestDistance;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/140-159/Problem153/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem153
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase("dog cat hello cat dog dog hello cat world", "hello", "world", 1)]
+		public void TestFindMajorityElement_ShouldReturnCorrectly(string text, string word, string otherWord, int expectedResult)
+		{
+			// Arrange, Act
+			var result = Solution.FindSmallestDistanceBetweenWords(text, word, otherWord);
+
+			// Assert
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
