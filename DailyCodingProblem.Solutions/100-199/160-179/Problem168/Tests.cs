3b71eeff8d7374ce19ebde7fa6e472db85f02b25
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-05-07 13:13:08 +0300

    Write tests

---
+++ DailyCodingProblem.Solutions/100-199/160-179/Problem168/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem168
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestRotate_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+			var expectedResult = new int[3, 3] { { 7, 4, 1 }, { 8, 5, 2 }, { 9, 6, 3 } };
+
+			// Act
+			var result = Solution.Rotate(input);
+
+			// Assert
+			CollectionAssert.AreEqual(expectedResult, result);
+		}
+	}
+}
