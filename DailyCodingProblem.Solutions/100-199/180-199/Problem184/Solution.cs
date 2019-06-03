c10697a31d658d4e34fc08241ef5690c597a08ff
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-06-03 11:41:47 +0300

    Solve problem 184

---
+++ DailyCodingProblem.Solutions/100-199/180-199/Problem184/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem184
+{
+	public class Solution
+	{
+		public static int FindGreatestCommonDenominator(int[] numbers)
+		{
+			var greatestDenominator = numbers.Min();
+
+			while (greatestDenominator > 0)
+			{
+				if (IsDenominator(numbers, greatestDenominator))
+				{
+					return greatestDenominator;
+				}
+			}
+
+			return greatestDenominator;
+		}
+
+		private static bool IsDenominator(int[] numbers, int number)
+		{
+			return numbers
+				.All(n => n % number == 0);
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/180-199/Problem184/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem184
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TesFindGreatestCommonDenominator_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new[] { 42, 56, 14};
+			var expectedResult = 14;
+
+			// Act
+			var result = Solution.FindGreatestCommonDenominator(input);
+
+			// Assert
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
