b66f9aaa29582b867cbfdcc6861e2466c1431bfb
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-06-10 08:53:36 +0300

    Solve problem 195 naive way

---
+++ DailyCodingProblem.Solutions/100-199/180-199/Problem195/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace DailyCodingProblem.Solutions.Problem195
+{
+	public class Solution
+	{
+		public static int FindNumberOfElementsBetween(int[][] matrix, int i1, int j1, int i2, int j2)
+		{
+			var itemsCount = 0;
+
+			for (int i = 0; i < matrix.Length; i++)
+			{
+				for (int j = 0; j < matrix[i].Length; j++)
+				{
+					if(matrix[i][j] < matrix[i1][j1] || matrix[i][j] > matrix[i2][j2])
+					{
+						itemsCount++;
+					}
+				}
+			}
+
+			return itemsCount;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/180-199/Problem195/Tests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem195
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TesFindNumberOfElementsBetween_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new[] {
+				new int[] {1, 3, 7, 10, 15, 20 },
+				new int[] {2, 6, 9, 14, 22, 25 },
+				new int[] { 3, 8, 10, 15, 25, 30},
+				new int[] {10, 11, 12, 23, 30, 35 },
+				new int[] {20, 25, 30, 35, 40, 45 },
+			};
+
+			var i1 = 1;
+			var j1 = 1;
+			var i2 = 3;
+			var j2 = 3;
+
+			var expectedResult = 14;
+
+			// Act
+			var result = Solution.FindNumberOfElementsBetween(input, i1, j1, i2, j2);
+
+			// Assert
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
