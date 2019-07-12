72745eb96d67a9ea1eb6760a412fc6711fcae4b9
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-07-12 14:04:47 +0300

    Add problem 232

---
+++ DailyCodingProblem.Solutions/200-299/220-239/Problem232/PrefixMapSum.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace DailyCodingProblem.Solutions.Problem232
+{
+	public class PrefixMapSum
+	{
+		private class TrieNode
+		{
+			public TrieNode(char character)
+			{
+				this.Character = character;
+				this.Sum = Sum;
+				this.Children = new Dictionary<char, TrieNode>();
+			}
+
+			public char Character { get; set; }
+
+			public int Sum { get; set; }
+
+			public IDictionary<char, TrieNode> Children { get; set; }
+		}
+
+		private readonly IDictionary<char, TrieNode> trieNodes = new Dictionary<char, TrieNode>();
+
+		public void Insert(string key, int value)
+		{
+			var nodes = trieNodes;
+
+			foreach (var character in key)
+			{
+				if (!nodes.ContainsKey(character))
+				{
+					nodes.Add(character, new TrieNode(character));
+				}
+
+				var node = nodes[character];
+				node.Sum += value;
+
+				nodes = node.Children;
+			}
+		}
+
+		public int Sum(string prefix)
+		{
+			var nodes = trieNodes;
+			var node = (TrieNode)null;
+
+			foreach (var character in prefix)
+			{
+				if (nodes.ContainsKey(character))
+				{
+					node = nodes[character];
+					nodes = nodes[character].Children;
+				}
+			}
+
+			return node?.Sum ?? 0;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/220-239/Problem232/Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem232
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestSum_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var mapsum = new PrefixMapSum();
+			mapsum.Insert("columnar", 3);
+			mapsum.Insert("column", 2);
+
+			var expectedResult = 5;
+
+			// Act
+			var sum = mapsum.Sum("col");
+
+			// Assert
+			Assert.AreEqual(expectedResult, sum);
+		}
+	}
+}
