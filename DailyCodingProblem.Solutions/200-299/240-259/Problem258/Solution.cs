4efa978f403f8030ec69114dd6e7018033e93b3a
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-08-05 16:01:29 +0300

    Add problem 258

---
+++ DailyCodingProblem.Solutions/200-299/240-259/Problem258/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DailyCodingProblem.Solutions.Common;
+using Moq;
+
+namespace DailyCodingProblem.Solutions.Problem258
+{
+	public class Solution
+	{
+		private static IDictionary<int, List<Node<int>>> levels;
+
+		public static int[] GetBoustrophedonText(Node<int> root)
+		{
+			var result = new List<int>();
+
+			levels = new Dictionary<int, List<Node<int>>>();
+
+			PopulateLevels(root);
+
+			TraverseBoustrophedon(result);
+
+			return result.ToArray();
+		}
+
+		private static void PopulateLevels(Node<int> current, int level = 1)
+		{
+			if (current == null)
+			{
+				return;
+			}
+
+			if (!levels.ContainsKey(level))
+			{
+				levels.Add(level, new List<Node<int>>());
+			}
+
+			levels[level].Add(current);
+
+			PopulateLevels(current.Left, level + 1);
+			PopulateLevels(current.Right, level + 1);
+		}
+
+		private static void TraverseBoustrophedon(List<int> values, bool fromLeft = true)
+		{
+			var level = 1;
+			while (level <= levels.Max(l => l.Key))
+			{
+				var toAdd = levels[level].Select(c => c.Value).ToList();
+
+				if (!fromLeft)
+				{
+					toAdd.Reverse();
+				}
+
+				values.AddRange(toAdd);
+
+				level++;
+				fromLeft = !fromLeft;
+			}
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/240-259/Problem258/Tests.cs
@@ -0,0 +1,30 @@
+﻿using DailyCodingProblem.Solutions.Common;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem258
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestGetBoustrophedonText_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var root = new Node<int>(1,
+				new Node<int>(2,
+					new Node<int>(4),
+					new Node<int>(5)),
+				new Node<int>(3,
+					new Node<int>(6),
+					new Node<int>(7)));
+
+			var expectedResult = new int[] { 1, 3, 2, 4, 5, 6, 7 };
+
+			// Act
+			var actual = Solution.GetBoustrophedonText(root);
+
+			// Assert
+			CollectionAssert.AreEqual(expectedResult, actual);
+		}
+	}
+}
