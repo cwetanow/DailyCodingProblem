83d54daab711bc708020dfe761c0d13d87e0fcbc
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-08-06 09:03:17 +0300

    Add problem 259

---
+++ DailyCodingProblem.Solutions/200-299/240-259/Problem259/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DailyCodingProblem.Solutions.Common;
+using Moq;
+
+namespace DailyCodingProblem.Solutions.Problem259
+{
+	public class Solution
+	{
+		public static IEnumerable<char> GetStartLetters(ICollection<string> words)
+		{
+			var result = new List<char>();
+
+			var startLetters = words
+				.Where(w => w.Length % 2 == 0 && words.Where(word => word[0] == w[0]).ToList().Count == 1)
+				.Select(w => w[0]);
+
+			result.AddRange(startLetters);
+
+			return result;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/240-259/Problem259/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using DailyCodingProblem.Solutions.Common;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem259
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase(1, new[] { "cat", "calf", "dog", "bear" }, new[] { 'b' })]
+		public void TestGetStartLetters_ShouldReturnCorrectly(int id, string[] letters, char[] expectedResult)
+		{
+			// Arrange, Act
+			var actualResult = Solution.GetStartLetters(letters);
+
+			// Assert
+			CollectionAssert.AreEquivalent(expectedResult, actualResult);
+		}
+	}
+}
