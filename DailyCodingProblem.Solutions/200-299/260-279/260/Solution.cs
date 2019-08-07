3305b9126477a5361d19b4b0575c1888895321d2
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-08-07 08:54:05 +0300

    Solve problem 260

---
+++ DailyCodingProblem.Solutions/200-299/260-279/260/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem260
+{
+	public class Solution
+	{
+		public const char Greater = '+';
+		public const char Lesser = '-';
+
+		public static int[] ReconstructArray(char[] clues)
+		{
+			var n = clues.Length;
+
+			var firstNumber = clues
+				.Count(c => c == Lesser);
+
+			var result = new int[n];
+
+			result[0] = firstNumber;
+			var greaterNumber = firstNumber + 1;
+			var lesserNumber = 0;
+
+			for (int i = 1; i < n; i++)
+			{
+				if (clues[i] == Greater)
+				{
+					result[i] = greaterNumber++;
+				}
+				else
+				{
+					result[i] = lesserNumber++;
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/260-279/260/Tests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem260
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestReconstructArray_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var clues = new[]
+			{
+				' ', '+', '+', '-', '+'
+			};
+
+			// Act
+			var reconstruction = Solution.ReconstructArray(clues);
+
+			// Assert
+			Assert.IsTrue(IsReconstructionValid(clues, reconstruction));
+		}
+
+		private bool IsReconstructionValid(char[] clues, int[] reconstruction)
+		{
+			for (int i = 1; i < reconstruction.Length; i++)
+			{
+				if (clues[i] == Solution.Greater && reconstruction[i] <= reconstruction[0])
+				{
+					return false;
+				}
+
+				if (clues[i] == Solution.Lesser && reconstruction[i] >= reconstruction[0])
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
