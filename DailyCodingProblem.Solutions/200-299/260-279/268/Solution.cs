f618e77155397565ab7b6667670f552f3ce29fe7
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-08-20 10:29:31 +0300

    #268

---
+++ DailyCodingProblem.Solutions/200-299/260-279/268/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DailyCodingProblem.Solutions.Problem268
+{
+	public class Solution
+	{
+		public static bool IsPowerOf(int number, int poweredNumber)
+		{
+			return Math.Log(number, poweredNumber) % 1 == 0;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/260-279/268/Tests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem268
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase(16, 4)]
+		[TestCase(64, 4)]
+		[TestCase(256, 4)]
+		public void TestIsPowerOf_NumberIsPower_ShouldReturnTrue(int number, int poweredNumber)
+		{
+			// Arrange, Act
+			var result = Solution.IsPowerOf(number, poweredNumber);
+
+			// Assert
+			Assert.IsTrue(result);
+		}
+
+		[TestCase(32, 4)]
+		[TestCase(128, 4)]
+		[TestCase(11, 4)]
+		public void TestIsPowerOf_NumberIsNotPower_ShouldReturnFalse(int number, int poweredNumber)
+		{
+			// Arrange, Act
+			var result = Solution.IsPowerOf(number, poweredNumber);
+
+			// Assert
+			Assert.IsFalse(result);
+		}
+	}
+}
