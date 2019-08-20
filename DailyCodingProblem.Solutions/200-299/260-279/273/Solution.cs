9ef8a2dc20c7f558328053d30a110641ee8a5357
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-08-20 10:16:26 +0300

    Solve problem 273

---
+++ DailyCodingProblem.Solutions/200-299/260-279/273/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem273
+{
+	public class Solution
+	{
+		public static int? GetFixedPoint(int[] input)
+		{
+			var fixedPoints = input
+				.Where((element, index) => element == index)
+				.ToList();
+
+			if (fixedPoints.Any())
+			{
+				return fixedPoints[0];
+			}
+
+			return null;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/260-279/273/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem273
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestGetFixedPoint_ThereIsFixedPoint_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new[] { -6, 0, 2, 40 };
+			var expectedResult = 2;
+
+			// Act
+			var actual = Solution.GetFixedPoint(input);
+
+			// Assert
+			Assert.AreEqual(expectedResult, actual);
+		}
+
+		[Test]
+		public void TestGetFixedPoint_ThereIsNoFixedPoint_ShouldReturnNull()
+		{
+			// Arrange
+			var input = new[] { 1, 5, 7, 8 };
+
+			// Act
+			var actual = Solution.GetFixedPoint(input);
+
+			// Assert
+			Assert.IsNull(actual);
+		}
+	}
+}
