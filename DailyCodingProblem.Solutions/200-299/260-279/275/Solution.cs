7be36ded040f6bba970821d6f4f4d71b8c42a34f
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-08-22 11:58:36 +0300

    #275

---
+++ DailyCodingProblem.Solutions/200-299/260-279/275/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace DailyCodingProblem.Solutions.Problem275
+{
+	public class Solution
+	{
+		private const char InitalChar = '_';
+
+		public static string GetNthTermOfSequence(int n)
+		{
+			var result = "1";
+
+			for (var i = 0; i < n - 1; i++)
+			{
+				result = GetNextTerm(result);
+			}
+
+			return result;
+		}
+
+		private static string GetNextTerm(string term)
+		{
+			var counter = 0;
+
+			var builder = new StringBuilder();
+
+			var previous = InitalChar;
+			var current = InitalChar;
+
+			for (var i = 0; i < term.Length; i++)
+			{
+				current = term[i];
+
+				if (current != previous && previous != InitalChar)
+				{
+					builder.Append(counter);
+					builder.Append(term[i - 1]);
+
+					counter = 1;
+				}
+				else
+				{
+					counter++;
+				}
+
+				previous = current;
+			}
+
+			builder.Append(counter);
+			builder.Append(current);
+
+			return builder.ToString();
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/260-279/275/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem275
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase(1, "1")]
+		[TestCase(4, "1211")]
+		[TestCase(5, "111221")]
+		public void TestGetNthTermOfSequence_ShouldReturnCorrectly(int n, string expectedResult)
+		{
+			// Arrange, Act
+			var actual = Solution.GetNthTermOfSequence(n);
+
+			// Assert
+			Assert.AreEqual(expectedResult, actual);
+		}
+	}
+}
