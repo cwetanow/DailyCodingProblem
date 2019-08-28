95386c7084c6027d39c291d993bd2c9a766b1ad0
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-08-28 16:18:50 +0300

    Solve problem 267

---
+++ DailyCodingProblem.Solutions/200-299/260-279/267/Solution.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DailyCodingProblem.Solutions.Problem267
+{
+	public class Solution
+	{
+		private static IDictionary<char, Func<char[][], int, int, (int, int), bool>> CheckFunctions = new Dictionary<char, Func<char[][], int, int, (int, int), bool>>();
+
+		private const char King = 'K';
+		private const char Queen = 'Q';
+		private const char Bishop = 'B';
+		private const char Pawn = 'P';
+		private const char Rook = 'R';
+		private const char Night = 'N';
+		private const char Empty = '.';
+
+		private const int BoardSize = 8;
+
+		static Solution()
+		{
+			CheckFunctions.Add(Queen, CheckQueen);
+			CheckFunctions.Add(Bishop, CheckBishop);
+			CheckFunctions.Add(Pawn, CheckPawn);
+			CheckFunctions.Add(Rook, CheckRook);
+			CheckFunctions.Add(Night, CheckNight);
+		}
+
+		public static bool IsKingInCheck(char[][] board)
+		{
+			var kingPosition = FindKingPosition(board);
+
+			for (var row = 0; row < BoardSize; row++)
+			{
+				for (var col = 0; col < BoardSize; col++)
+				{
+					if (board[row][col] != Empty && board[row][col] != King)
+					{
+						var checkFunction = CheckFunctions[board[row][col]];
+
+						if (checkFunction(board, row, col, kingPosition))
+						{
+							return true;
+						}
+					}
+				}
+			}
+
+			return false;
+		}
+
+		private static bool CheckQueen(char[][] board, int row, int col, (int row, int col) kingPosition)
+		{
+			return CheckBishop(board, row, col, kingPosition) || CheckRook(board, row, col, kingPosition);
+		}
+
+		private static bool CheckBishop(char[][] board, int row, int col, (int row, int col) kingPosition)
+		{
+			for (var i = -BoardSize; i < BoardSize; i++)
+			{
+				if (row + i == kingPosition.row && col + i == kingPosition.col)
+				{
+					return true;
+				}
+			}
+
+
+			for (var i = -BoardSize; i < BoardSize; i++)
+			{
+				if (row - i == kingPosition.row && col + i == kingPosition.col)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		private static bool CheckRook(char[][] board, int row, int col, (int row, int col) kingPosition)
+		{
+			return row == kingPosition.row || col == kingPosition.col;
+		}
+
+		private static bool CheckPawn(char[][] board, int row, int col, (int row, int col) kingPosition)
+		{
+			return row == kingPosition.row + 1 && (col == kingPosition.col - 1 || col == kingPosition.col + 1);
+		}
+
+		private static bool CheckNight(char[][] board, int row, int col, (int row, int col) kingPosition)
+		{
+			return
+				(row == kingPosition.row + 1 && col == kingPosition.col - 2) ||
+				(row == kingPosition.row + 2 && col == kingPosition.col - 1) ||
+
+				(row == kingPosition.row - 1 && col == kingPosition.col - 2) ||
+				(row == kingPosition.row - 2 && col == kingPosition.col - 1) ||
+
+				(row == kingPosition.row - 1 && col == kingPosition.col + 2) ||
+				(row == kingPosition.row - 2 && col == kingPosition.col + 1) ||
+
+				(row == kingPosition.row + 1 && col == kingPosition.col + 2) ||
+				(row == kingPosition.row + 2 && col == kingPosition.col + 1);
+		}
+
+		private static (int row, int col) FindKingPosition(char[][] board)
+		{
+			for (var row = 0; row < BoardSize; row++)
+			{
+				for (var col = 0; col < BoardSize; col++)
+				{
+					if (board[row][col] == King)
+					{
+						return (row, col);
+					}
+				}
+			}
+
+			return (-1, -1);
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/260-279/267/Tests.cs
@@ -0,0 +1,168 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem267
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestIsKingInCheck_BishopIsHitting_ShouldReturnTrue()
+		{
+			// Arrange
+			var board = new[]
+			{
+				new [] { '.', '.', '.', 'K', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', 'B', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', 'P','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','R' },
+				new [] { '.', '.', 'N', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', 'Q', '.','.' }
+			};
+
+			// Act
+			var isInCheck = Solution.IsKingInCheck(board);
+
+			// Assert
+			Assert.IsTrue(isInCheck);
+		}
+		[Test]
+		public void TestIsKingInCheck_RookIsHitting_ShouldReturnTrue()
+		{
+			// Arrange
+			var board = new[]
+			{
+				new [] { '.', '.', '.', 'K', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', 'P','.' },
+				new [] { '.', '.', '.', 'R', '.', '.', '.','R' },
+				new [] { '.', '.', 'N', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', 'Q', '.','.' }
+			};
+
+			// Act
+			var isInCheck = Solution.IsKingInCheck(board);
+
+			// Assert
+			Assert.IsTrue(isInCheck);
+		}
+
+		[Test]
+		public void TestIsKingInCheck_NightIsHitting_ShouldReturnTrue()
+		{
+			// Arrange
+			var board = new[]
+			{
+				new [] { '.', '.', '.', 'K', '.', '.', '.','.' },
+				new [] { '.', 'N', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', 'P','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','R' },
+				new [] { '.', '.', 'N', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', 'Q', '.','.' }
+			};
+
+			// Act
+			var isInCheck = Solution.IsKingInCheck(board);
+
+			// Assert
+			Assert.IsTrue(isInCheck);
+		}
+
+		[Test]
+		public void TestIsKingInCheck_QueenIsHittingDiagonally_ShouldReturnTrue()
+		{
+			// Arrange
+			var board = new[]
+			{
+				new [] { '.', '.', '.', 'K', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', 'Q', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', 'P','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','R' },
+				new [] { '.', '.', 'N', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', 'Q', '.','.' }
+			};
+
+			// Act
+			var isInCheck = Solution.IsKingInCheck(board);
+
+			// Assert
+			Assert.IsTrue(isInCheck);
+		}
+
+		[Test]
+		public void TestIsKingInCheck_QueenIsHittingByRow_ShouldReturnTrue()
+		{
+			// Arrange
+			var board = new[]
+			{
+				new [] { '.', 'Q', '.', 'K', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', 'P','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','R' },
+				new [] { '.', '.', 'N', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', 'Q', '.','.' }
+			};
+
+			// Act
+			var isInCheck = Solution.IsKingInCheck(board);
+
+			// Assert
+			Assert.IsTrue(isInCheck);
+		}
+
+		[Test]
+		public void TestIsKingInCheck_PawnIsHitting_ShouldReturnTrue()
+		{
+			// Arrange
+			var board = new[]
+			{
+				new [] { '.', '.', '.', 'K', '.', '.', '.','.' },
+				new [] { '.', '.', 'P', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', 'P','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','R' },
+				new [] { '.', '.', 'N', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', 'Q', '.','.' }
+			};
+
+			// Act
+			var isInCheck = Solution.IsKingInCheck(board);
+
+			// Assert
+			Assert.IsTrue(isInCheck);
+		}
+
+		[Test]
+		public void TestIsKingInCheck_IsNotInCheck_ShouldReturnFalse()
+		{
+			// Arrange
+			var board = new[]
+			{
+				new [] { '.', '.', '.', 'K', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { 'B', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', 'P','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','R' },
+				new [] { '.', '.', 'N', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', '.', '.','.' },
+				new [] { '.', '.', '.', '.', '.', 'Q', '.','.' }
+			};
+
+			// Act
+			var isInCheck = Solution.IsKingInCheck(board);
+
+			// Assert
+			Assert.IsFalse(isInCheck);
+		}
+	}
+}
