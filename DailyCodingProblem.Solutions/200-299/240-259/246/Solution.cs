0006caae8dd93e76d56222da5160c2502e955929
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-08-28 16:54:09 +0300

    Solve #246

---
+++ DailyCodingProblem.Solutions/200-299/240-259/246/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem246
+{
+	public class Solution
+	{
+		public static bool CanWordsBeChained(IList<string> words)
+		{
+			return words
+				.Any(word => CanWordsBeChained(words.Where(w => w != word).ToList(), word, word));
+		}
+
+		private static bool CanWordsBeChained(IList<string> remainingWords, string currentWord, string firstWord)
+		{
+			if (!remainingWords.Any() && currentWord.Last() == firstWord.First())
+			{
+				return true;
+			}
+
+			var nextPossibleWords = remainingWords
+				.Where(word => word.StartsWith(currentWord.Last()))
+				.ToList();
+
+			return nextPossibleWords
+				.Any(word => CanWordsBeChained(remainingWords.Where(w => w != word).ToList(), word, firstWord));
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/240-259/246/Tests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem246
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestCanWordsBeChained_CanBeChained_ShouldReturnTrue()
+		{
+			// Arrange
+			var words = new List<string> {
+				"chair", "height", "racket", "touch", "tunic"
+			};
+
+			// Act
+			var result = Solution.CanWordsBeChained(words);
+
+			// Assert
+			Assert.IsTrue(result);
+		}
+
+		[Test]
+		public void TestCanWordsBeChained_CanNotBeChained_ShouldReturnFalse()
+		{
+			// Arrange
+			var words = new List<string> {
+				"chair", "height", "racket"
+			};
+
+			// Act
+			var result = Solution.CanWordsBeChained(words);
+
+			// Assert
+			Assert.IsFalse(result);
+		}
+	}
+}
