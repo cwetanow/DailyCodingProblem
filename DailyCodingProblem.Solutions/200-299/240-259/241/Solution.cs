75cbda129371373dbeddda190e1bcd360fa9e5a4
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-08-28 16:45:04 +0300

    Add problem 241

---
+++ DailyCodingProblem.Solutions/200-299/240-259/241/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem241
+{
+	public class Solution
+	{
+		public static int GetHIndex(int[] paperCitations)
+		{
+			var n = paperCitations.Length;
+			var index = 0;
+
+			for (var i = 1; i <= n; i++)
+			{
+				if (HasIndex(i, paperCitations, n))
+				{
+					index = i;
+				}
+			}
+
+			return index;
+		}
+
+		public static bool HasIndex(int index, int[] citations, int n)
+		{
+			return citations
+					   .Where(paperCitations => paperCitations >= index)
+					   .ToList()
+					   .Count >= index;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/240-259/241/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem241
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase(3, new[] { 4, 3, 0, 1, 5 })]
+		public void TestGetHIndex_ShouldReturnCorrectly(int expectedResult, int[] paperCitations)
+		{
+			// Arrange, Act
+			var index = Solution.GetHIndex(paperCitations);
+
+			// Assert
+			Assert.AreEqual(expectedResult, index);
+		}
+	}
+}
