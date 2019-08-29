1e91c8bb8085ed67474b9e5d8e216ce0f2ee3189
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-08-29 09:36:06 +0300

    Solve #263

---
+++ DailyCodingProblem.Solutions/200-299/260-279/263/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem263
+{
+	public class Solution
+	{
+		private static readonly char[] Separators = { ',', ';', ':' };
+		private static readonly char[] TerminalMarks = { '.', '!', '?' };
+
+		private const char Space = ' ';
+
+		public static bool IsValidSentence(string sentence)
+		{
+			if (sentence.Length < 2)
+			{
+				return false;
+			}
+
+			// The sentence must start with a capital letter,
+			if (!char.IsUpper(sentence.First()))
+			{
+				return false;
+			}
+
+			// followed by a lowercase letter or a space.
+			if (sentence[1] != Space && !char.IsLower(sentence[1]))
+			{
+				return false;
+			}
+
+			// All other characters must be lowercase letters, separators (,,;,:) or terminal marks (.,?,!,‽).
+			if (sentence.Skip(1).Any(c => !char.IsLower(c) && !Separators.Contains(c) && !TerminalMarks.Contains(c) && c != Space))
+			{
+				return false;
+			}
+
+			// There must be a single space between each word.
+			if (sentence.Skip(1).Where((c, index) => c == Space && sentence[index] == Space).Any())
+			{
+				return false;
+			}
+
+
+			//	The sentence must end with a terminal mark immediately following a word.
+			if (!TerminalMarks.Contains(sentence.Last()) || !char.IsLetter(sentence[sentence.Length - 2]) || !char.IsLower(sentence[sentence.Length - 2]))
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/260-279/263/Tests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem263
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestIsValidSentence_IsValid_ShouldReturnTrue()
+		{
+			// Arrange
+			var sentence = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+			// Act
+			var result = Solution.IsValidSentence(sentence);
+
+			// Assert
+			Assert.IsTrue(result);
+		}
+
+		[TestCase(1, "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")]
+		[TestCase(1, "LOrem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")]
+		[TestCase(1, "Lorem ipsum dolor sit amEt, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")]
+		[TestCase(1, "Lorem ipsum dolor  sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")]
+		[TestCase(1, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua")]
+		public void TestIsValidSentence_IsNotValid_ShouldReturnFalse(int randomNum, string invalidSentence)
+		{
+			// Arrange, Act
+			var result = Solution.IsValidSentence(invalidSentence);
+
+			// Assert
+			Assert.IsFalse(result);
+		}
+	}
+}
