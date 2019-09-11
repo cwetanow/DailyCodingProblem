be3d10e9947b703811d1841f1b27fac12818f9b3
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-09-11 15:50:53 +0300

    Solve problem 295

---
+++ DailyCodingProblem.Solutions/200-299/280-299/295/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DailyCodingProblem.Solutions.Problem295
+{
+	public class Solution
+	{
+		public static int[] GetKthRow(int k)
+		{
+			var result = new int[k];
+			result[0] = 1;
+
+			for (var row = 1; row < k; row++)
+			{
+				result[row] = 1;
+				for (var i = row - 1; i > 0; i--)
+				{
+					result[i] = result[i] + result[i - 1];
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/280-299/295/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem295
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase(1, new[] { 1 })]
+		[TestCase(2, new[] { 1, 1 })]
+		[TestCase(3, new[] { 1, 2, 1 })]
+		[TestCase(4, new[] { 1, 3, 3, 1 })]
+		[TestCase(5, new[] { 1, 4, 6, 4, 1 })]
+		public void Test_ShouldReturnCorrectly(int k, int[] expectedResult)
+		{
+			// Arrange, Act
+			var result = Solution.GetKthRow(k);
+
+			// Assert
+			CollectionAssert.AreEqual(expectedResult, result);
+		}
+	}
+}
