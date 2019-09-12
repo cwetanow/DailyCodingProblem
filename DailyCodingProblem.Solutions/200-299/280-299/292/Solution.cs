7801b5d8252bd3f108cc658cd7391178fce84b76
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-09-12 09:11:29 +0300

    Solve #292

---
+++ DailyCodingProblem.Solutions/200-299/280-299/292/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem292
+{
+	public class Solution
+	{
+		public static IEnumerable<IEnumerable<int>> FindSatisfactoryPairOfTeams(IList<IList<int>> input)
+		{
+			var firstTeam = new List<int>();
+			var secondTeam = new List<int>();
+
+			for (var i = 0; i < input.Count; i++)
+			{
+				var studentEnemies = input[i];
+
+				if (!firstTeam.Any(student => studentEnemies.Contains(student)))
+				{
+					firstTeam.Add(i);
+				}
+				else if (!secondTeam.Any(student => studentEnemies.Contains(student)))
+				{
+					secondTeam.Add(i);
+				}
+				else
+				{
+					return null;
+				}
+			}
+
+			return new List<IList<int>> { firstTeam, secondTeam };
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/280-299/292/Tests.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem292
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestFindSatisfactoryPairOfTeams_HasSatisfactoryPair_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new List<IList<int>>
+			{
+				new List<int> { 3},
+				new List<int> {2 },
+				new List<int> { 1,4},
+				new List<int> {0,4,5 },
+				new List<int> { 2,3},
+				new List<int> { 3}
+			};
+
+			var expectedResult = new List<IList<int>>
+			{
+				new List<int>{0,1,4,5},
+				new List<int>{2,3}
+			};
+
+			// Act
+			var actual = Solution.FindSatisfactoryPairOfTeams(input);
+
+			// Assert
+			CollectionAssert.AreEquivalent(expectedResult, actual);
+		}
+
+
+		[Test]
+		public void TestFindSatisfactoryPairOfTeams_DoesNotHasSatisfactoryPair_ShouldReturnNull()
+		{
+			// Arrange
+			var input = new List<IList<int>>
+			{
+				new List<int> { 3},
+				new List<int> {2 },
+				new List<int> { 1,3,4},
+				new List<int> {0,2,4,5 },
+				new List<int> { 2,3},
+				new List<int> { 3}
+			};
+
+			// Act
+			var actual = Solution.FindSatisfactoryPairOfTeams(input);
+
+			// Assert
+			Assert.IsNull(actual);
+		}
+	}
+}
