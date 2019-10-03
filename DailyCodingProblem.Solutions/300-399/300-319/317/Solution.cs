8acdc0e35b7805e7f8f52601a2a00f791b01e864
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-10-03 13:48:05 +0300

    Add 317

---
+++ DailyCodingProblem.Solutions/300-399/300-319/317/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem317
+{
+	public class Solution
+	{
+		public static int GetBitwiseAnd(int m, int n)
+		{
+			if (n < m)
+			{
+				return GetBitwiseAnd(n, m);
+			}
+
+			return Enumerable
+				.Range(m, n - m + 1)
+				.Aggregate((acc, x) => acc & x);
+		}
+	}
+}
+
---
+++ DailyCodingProblem.Solutions/300-399/300-319/317/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem317
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase(4, 13, 0)]
+		public void TestGetBitwiseAnd_ShouldReturnCorrectly(int m, int n, int expectedResult)
+		{
+			// Arrange, Act
+			var actual = Solution.GetBitwiseAnd(m, n);
+
+			// Assert
+			Assert.AreEqual(expectedResult, actual);
+		}
+	}
+}
