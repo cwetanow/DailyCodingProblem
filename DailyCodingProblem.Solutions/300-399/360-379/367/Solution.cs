6631a4ec74716306191599dd914b97ce86b6385c
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-11-25 09:49:12 +0200

    Solve #367

---
+++ DailyCodingProblem.Solutions/300-399/360-379/367/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace DailyCodingProblem.Solutions.Problem367
+{
+	public class Solution
+	{
+		public static IEnumerable<int> MergeIterators(IEnumerator<int> first, IEnumerator<int> second)
+		{
+			var firstAny = first.MoveNext();
+			var secondAny = second.MoveNext();
+
+			while (firstAny || secondAny)
+			{
+				int current;
+
+				if (firstAny && secondAny)
+				{
+					if (first.Current < second.Current)
+					{
+						current = first.Current;
+						firstAny = first.MoveNext();
+					}
+					else
+					{
+						current = second.Current;
+						secondAny = second.MoveNext();
+					}
+				}
+				else if (firstAny)
+				{
+					current = first.Current;
+					firstAny = first.MoveNext();
+				}
+				else
+				{
+					current = second.Current;
+					secondAny = second.MoveNext();
+				}
+
+				yield return current;
+			}
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/360-379/367/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem367
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestMergeIterators_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var firstList = new List<int> { 5, 10, 15 };
+			var secondList = new List<int> { 3, 8, 9 };
+			var expectedResult = new List<int> { 3, 5, 8, 9, 10, 15 };
+
+			using (var firstIterator = firstList.GetEnumerator())
+			using (var secondIterator = secondList.GetEnumerator())
+			{
+				// Act
+				var actualResult = Solution.MergeIterators(firstIterator, secondIterator).ToList();
+
+				// Assert
+
+				actualResult.Should().BeEquivalentTo(expectedResult);
+			}
+		}
+	}
+}
