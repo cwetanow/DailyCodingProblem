e27937a8465478d5cbbc9112a85c75374298f1b8
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-11-25 09:58:30 +0200

    Solve #368

---
+++ DailyCodingProblem.Solutions/300-399/360-379/368/KeyValueStore.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace DailyCodingProblem.Solutions.Problem368
+{
+	public class KeyValueStore
+	{
+		private readonly IDictionary<int, int> store = new Dictionary<int, int>();
+		private readonly IDictionary<int, int> maxKeys = new Dictionary<int, int>();
+
+		public void Update(int key, int value)
+		{
+			if (store.ContainsKey(key))
+			{
+				store[key] = value;
+			}
+			else
+			{
+				store.Add(key, value);
+			}
+
+			if (maxKeys.ContainsKey(value) && maxKeys[value] < key)
+			{
+				maxKeys[value] = key;
+			}
+			else
+			{
+				maxKeys.Add(value, key);
+			}
+		}
+
+		public int? Get(int key)
+		{
+			if (store.TryGetValue(key, out var value))
+			{
+				return value;
+			}
+
+			return null;
+		}
+
+		public int? MaxKey(int value)
+		{
+			if (maxKeys.TryGetValue(value, out var key))
+			{
+				return key;
+			}
+
+			return null;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/360-379/368/Tests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem368
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestKeyValueStore_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var store = new KeyValueStore();
+			store.Update(1, 1);
+			store.Update(2, 1);
+
+			var expectedMaxKey = 2;
+
+			// Act
+			var actualMaxKey = store.MaxKey(1);
+
+			// Assert
+			actualMaxKey.Should().Be(expectedMaxKey);
+		}
+	}
+}
