048557cf9324f456ed7f1e74d25771a72f16e30b
Ivan Mladenov <mladenovv.ivan@gmail.com>
2019-11-26 09:18:42 +0200

    Solve #371

---
+++ DailyCodingProblem.Solutions/300-399/360-379/371/Solution.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem371
+{
+	public class Solution
+	{
+		private const char Plus = '+';
+		private const char Equal = '=';
+
+		public static IDictionary<string, int> GetVariablesWithValues(ICollection<string> equations)
+		{
+			var result = new Dictionary<string, int>();
+
+			while (equations.Any())
+			{
+				var next = equations.FirstOrDefault(eq => CanBeCalculated(eq, result));
+
+				if (string.IsNullOrEmpty(next))
+				{
+					return null;
+				}
+
+				Caclulate(next, result);
+				equations.Remove(next);
+			}
+
+			return result;
+		}
+
+		private static void Caclulate(string equation, IDictionary<string, int> variables)
+		{
+			equation = variables
+			   .Where(v => equation.Contains(v.Key))
+			   .Aggregate(equation, (current, variable) => current.Replace(variable.Key, variable.Value.ToString()));
+
+			var sides = equation.Split(Equal)
+				.Select(s => s.Trim())
+				.ToList();
+
+			// Variable on left side
+			if (!int.TryParse(sides[0], out _))
+			{
+				var variableValue = sides[1]
+					.Split(Plus)
+					.Select(s => s.Trim())
+					.Aggregate(0, (current, next) => current + int.Parse(next));
+
+				variables.Add(sides[0], variableValue);
+			}
+			// Variable on right side
+			else
+			{
+				var variableValue = sides[1]
+					.Split(Plus)
+					.Select(s => s.Trim())
+					.Where(s => int.TryParse(s, out _))
+					.Select(int.Parse)
+					.Aggregate(int.Parse(sides[0]), (current, next) => current - next);
+
+				var variable = sides[1]
+					.Split(Plus)
+					.Select(s => s.Trim())
+					.Single(s => !int.TryParse(s, out _));
+
+				variables.Add(variable, variableValue);
+			}
+		}
+
+		private static bool CanBeCalculated(string equation, IDictionary<string, int> variables)
+		{
+			var splitted = equation.Split(Plus)
+				.SelectMany(s => s.Split(Equal))
+				.Select(s => s.Trim())
+				.ToList();
+
+			splitted = splitted
+				.Where(s => !int.TryParse(s, out _))
+				.ToList();
+
+			splitted = splitted
+				.Where(s => !variables.ContainsKey(s))
+				.ToList();
+
+			if (splitted.Count == 1)
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/360-379/371/Tests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem371
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestGetVariablesWithValues_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var equations = new List<string>
+			{
+				"y = x + 1",
+				"5 = x + 3",
+				"10 = z + y + 2"
+			};
+
+			var expected = new Dictionary<string, int>
+			{
+				{"x",2 },
+				{"y",3 },
+				{"z",5 }
+			};
+
+			// Act
+			var actual = Solution.GetVariablesWithValues(equations);
+
+			// Assert
+			actual.Should().BeEquivalentTo(expected);
+		}
+
+
+		[Test]
+		public void TestGetVariablesWithValues_Impossible_ShouldReturnNull()
+		{
+			// Arrange
+			var equations = new List<string>
+			{
+				"10 = z + y + 2"
+			};
+
+			// Act
+			var actual = Solution.GetVariablesWithValues(equations);
+
+			// Assert
+			actual.Should().BeNull();
+		}
+	}
+}
