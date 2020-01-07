c09bb004ab8f5f1d1a7aeaa30862df0a8d323385
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-07 15:02:41 +0200

    Solve problem 363

---
+++ DailyCodingProblem.Solutions/300-399/360-379/363/AddSubtractBuilder.cs
@@ -0,0 +1,28 @@
+﻿namespace DailyCodingProblem.Solutions.Problem363
+{
+	public class AddSubtractBuilder
+	{
+		private int result;
+		private bool isAddNext = true;
+
+		private AddSubtractBuilder(int initialNumber)
+		{
+			this.result = initialNumber;
+		}
+
+		public AddSubtractBuilder Append(int number)
+		{
+			result += isAddNext ? number : -number;
+			isAddNext = !isAddNext;
+
+			return this;
+		}
+
+		public int Compute()
+		{
+			return this.result;
+		}
+
+		public static AddSubtractBuilder Create(int initialNumber) => new AddSubtractBuilder(initialNumber);
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/360-379/363/Tests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem363
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase(7, 7)]
+		[TestCase(1, 0, 2, 3)]
+		[TestCase(-5, 11, 10, 3, 9)]
+		public void Test_ShouldReturnCorrectly(int initialNumber, int expectedResult, params int[] otherNumbers)
+		{
+			// Arrange
+			var builder = AddSubtractBuilder.Create(initialNumber);
+
+			foreach (var otherNumber in otherNumbers)
+			{
+				builder.Append(otherNumber);
+			}
+			
+			// Act
+			var actualResut = builder.Compute();
+
+			// Assert
+			actualResut.Should().Be(expectedResult);
+		}
+	}
+}
