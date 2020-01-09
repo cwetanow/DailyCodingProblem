fdd624197fb2550afcdfa7621750e930d52bbb05
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-09 11:38:16 +0200

    Solve #369

---
+++ DailyCodingProblem.Solutions/300-399/360-379/369/StockTracker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Wintellect.PowerCollections;
+
+namespace DailyCodingProblem.Solutions.Problem369
+{
+	public class StockTracker
+	{
+		private readonly OrderedBag<double> Ascending = new OrderedBag<double>((a, b) => a.CompareTo(b));
+		private readonly OrderedBag<double> Descending = new OrderedBag<double>((a, b) => -a.CompareTo(b));
+
+		private readonly IDictionary<int, double> prices = new Dictionary<int, double>();
+
+		private double Sum { get; set; } = 0;
+
+		public void Add(int datestamp, double price)
+		{
+			prices.Add(datestamp, price);
+
+			Ascending.Add(price);
+			Descending.Add(price);
+
+			Sum += price;
+		}
+
+		public void Update(int datestamp, double price)
+		{
+			Remove(datestamp);
+			Add(datestamp, price);
+		}
+
+		public void Remove(int datestamp)
+		{
+			if (!prices.ContainsKey(datestamp))
+			{
+				return;
+			}
+
+			var oldPrice = prices[datestamp];
+			prices.Remove(datestamp);
+
+			Ascending.Remove(oldPrice);
+			Descending.Remove(oldPrice);
+
+			Sum -= oldPrice;
+		}
+
+		public double Min => Ascending.GetFirst();
+		public double Max => Descending.GetFirst();
+		public double Average => prices.Count > 0 ? Sum / prices.Count : 0;
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/360-379/369/Tests.cs
@@ -0,0 +1,97 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem369
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestMin_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var tracker = new StockTracker();
+			tracker.Add(1, 2);
+			tracker.Add(2, 1);
+			tracker.Add(3, 3);
+
+			var expectedResult = 1;
+
+			// Act
+			var actual = tracker.Min;
+
+			// Assert
+			actual.Should().Be(expectedResult);
+		}
+
+		[Test]
+		public void TestMax_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var tracker = new StockTracker();
+			tracker.Add(1, 2);
+			tracker.Add(2, 1);
+			tracker.Add(3, 3);
+
+			var expectedResult = 3;
+
+			// Act
+			var actual = tracker.Max;
+
+			// Assert
+			actual.Should().Be(expectedResult);
+		}
+
+		[Test]
+		public void TestAverage_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var tracker = new StockTracker();
+			tracker.Add(1, 2);
+			tracker.Add(2, 1);
+			tracker.Add(3, 3);
+
+			var expectedResult = 2;
+
+			// Act
+			var actual = tracker.Average;
+
+			// Assert
+			actual.Should().Be(expectedResult);
+		}
+
+		[Test]
+		public void TestUpdate_ShouldUpdateCorrectly()
+		{
+			// Arrange
+			var tracker = new StockTracker();
+			tracker.Add(2, 1);
+			tracker.Update(2, 5);
+
+			var expectedResult = 5;
+
+			// Act
+			var actual = tracker.Average;
+
+			// Assert
+			actual.Should().Be(expectedResult);
+		}
+
+		[Test]
+		public void TestRemove_ShouldBehaveCorrectly()
+		{
+			// Arrange
+			var tracker = new StockTracker();
+			tracker.Add(2, 1);
+			tracker.Remove(2);
+
+			var expectedResult = 0;
+
+			// Act
+			var actual = tracker.Average;
+
+			// Assert
+			actual.Should().Be(expectedResult);
+		}
+	}
+}
