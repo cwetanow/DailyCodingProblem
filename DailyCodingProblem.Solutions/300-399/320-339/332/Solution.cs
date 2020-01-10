caa84605c593bbcf5c1b3704bb6217cb835821a1
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-10 09:28:14 +0200

    Solve 332

---
+++ DailyCodingProblem.Solutions/300-399/320-339/332/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyCodingProblem.Solutions.Problem332
+{
+	public class Solution
+	{
+		public static int GetNumberOfPairs(int m, int n)
+		{
+			var result = 0;
+
+			for (var i = 1; i < m; i++)
+			{
+				var xor = i ^ (m - i);
+				if ( xor== n)
+				{
+					result++;
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/320-339/332/Tests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem332
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase(5, 5, 2)]
+		public void TestGetNumberOfPairs_ShouldReturnCorrectly(int m, int n, int expectedResult)
+		{
+			// Arrange, Act
+			var actualResult = Solution.GetNumberOfPairs(m, n);
+
+			// Assert
+			actualResult.Should().Be(expectedResult);
+		}
+	}
+}
