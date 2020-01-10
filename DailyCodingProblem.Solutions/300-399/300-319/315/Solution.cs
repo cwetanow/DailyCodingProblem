04bdbdff66d017c36644fc467d3e5727aaa32443
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-10 13:55:36 +0200

    Solve #315

---
+++ DailyCodingProblem.Solutions/300-399/300-319/315/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace DailyCodingProblem.Solutions.Problem315
+{
+	public class Solution
+	{
+		public static bool IsToeplitzMatrix(int[][] matrix, int rows, int cols)
+		{
+			var row = 0;
+			var col = cols - 1;
+
+			while (row != rows - 1 && col != 0)
+			{
+				var currentRow = row + 1;
+				var currentCol = col + 1;
+
+				var number = matrix[row][col];
+
+				while (currentRow < rows && currentCol < cols)
+				{
+					if (matrix[currentRow][currentCol] != number)
+					{
+						return false;
+					}
+
+					currentRow++;
+					currentCol++;
+				}
+
+				if (col > 0)
+				{
+					col--;
+				}
+				else
+				{
+					row++;
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/300-319/315/Tests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem315
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestIsToeplitzMatrix_SatisfiesCondition_ShouldReturnTrue()
+		{
+			// Arrange
+			var matrix = new[]
+			{
+				new []{ 1, 2, 3, 4, 8 },
+				new []{ 5, 1, 2, 3, 4 },
+				new []{ 4, 5, 1, 2, 3 },
+				new []{ 7, 4, 5, 1, 2 },
+			};
+
+			// Act
+			var actualResult = Solution.IsToeplitzMatrix(matrix, 4, 5);
+
+			// Assert
+			actualResult.Should().BeTrue();
+		}
+
+		[Test]
+		public void TestIsToeplitzMatrix_DoesnotSatisfyCondition_ShouldReturnTrue()
+		{
+			// Arrange
+			var matrix = new[]
+			{
+				new []{ 1, 2, 3, 4, 5 },
+				new []{ 5, 1, 2, 3, 5 },
+				new []{ 4, 5, 1, 2, 3 },
+				new []{ 7, 4, 5, 1, 2 },
+			};
+
+			// Act
+			var actualResult = Solution.IsToeplitzMatrix(matrix, 4, 5);
+
+			// Assert
+			actualResult.Should().BeFalse();
+		}
+	}
+}
