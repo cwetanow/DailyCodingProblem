f592308f44d48bb763c4012e1a9949d3ae9dee46
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-14 16:05:13 +0200

    Solve problem 202

---
+++ DailyCodingProblem.Solutions/200-299/200-219/202/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem202
+{
+	public class Solution
+	{
+		public static bool IsPalindrome(int number)
+		{
+			var queue = new Queue<int>();
+			var stack = new Stack<int>();
+
+			while (number > 0)
+			{
+				var nextDigit = number % 10;
+
+				queue.Enqueue(nextDigit);
+				stack.Push(nextDigit);
+
+				number /= 10;
+			}
+
+			while (queue.Any() && stack.Any())
+			{
+				if (queue.Dequeue() != stack.Pop())
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/200-219/202/Tests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem202
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase(121)]
+		[TestCase(888)]
+		public void TestIsPalindrome_IsPalindrome_ShouldReturnTrue(int number)
+		{
+			// Arrange, Act
+			var actualResult = Solution.IsPalindrome(number);
+
+			// Assert
+			actualResult.Should().BeTrue();
+		}
+
+		[TestCase(678)]
+		public void TestIsPalindrome_IsNotPalindrome_ShouldReturnFalse(int number)
+		{
+			// Arrange, Act
+			var actualResult = Solution.IsPalindrome(number);
+
+			// Assert
+			actualResult.Should().BeFalse();
+		}
+	}
+}
