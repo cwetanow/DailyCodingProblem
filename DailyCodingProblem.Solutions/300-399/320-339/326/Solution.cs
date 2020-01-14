c2fe0b788e2e6f0fd63fb116c1dcc17d7758314e
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-14 10:45:30 +0200

    Solve problem 326

---
+++ DailyCodingProblem.Solutions/300-399/320-339/326/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using DailyCodingProblem.Solutions.Common;
+
+namespace DailyCodingProblem.Solutions.Problem326
+{
+	public class Solution
+	{
+		public static Node<int> BuildCartesianTree(IList<int> input)
+		{
+			var root = (Node<int>)null;
+			var current = (Node<int>)null;
+
+			foreach (var number in input)
+			{
+				if (root == null)
+				{
+					root = new Node<int>(number);
+					current = root;
+				}
+				else if (current.Value > number)
+				{
+					current = new Node<int>(number, current);
+					root = current;
+				}
+				else if (current.Value < number)
+				{
+					current.Right = new Node<int>(number);
+				}
+			}
+
+			return root;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/320-339/326/Tests.cs
@@ -0,0 +1,32 @@
+﻿using DailyCodingProblem.Solutions.Common;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem326
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestBuildCartesianTree_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new[] { 3, 2, 6, 1, 9 };
+
+			var expectedResult = new Node<int>(
+				1,
+				new Node<int>(2,
+					new Node<int>(3),
+					new Node<int>(6)),
+				new Node<int>(9)
+			);
+
+			// Act
+			var actualResult = Solution.BuildCartesianTree(input);
+
+			// Assert
+			actualResult.Should().BeEquivalentTo(expectedResult, 
+				opts => opts.Excluding(c => c.Parent).IgnoringCyclicReferences());
+		}
+	}
+}
