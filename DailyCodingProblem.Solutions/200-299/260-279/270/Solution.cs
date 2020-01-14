aa23721240781f104b66d16b24dab6a63ab3445e
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-14 10:31:48 +0200

    Solve problem 270

---
+++ DailyCodingProblem.Solutions/200-299/260-279/270/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem270
+{
+	public class Solution
+	{
+		public static int GetMessagePropagationTime(IEnumerable<(int start, int finish, int time)> edges, int n)
+		{
+			var shortestPaths = Enumerable.Range(0, n + 1)
+				.Select(_ => int.MaxValue)
+				.ToList();
+
+			var visited = shortestPaths
+				.Select(_ => false)
+				.ToList();
+
+			GetShortestPaths(edges, n, shortestPaths, visited, 0, 0);
+
+			return shortestPaths.Max();
+		}
+
+		private static void GetShortestPaths(IEnumerable<(int start, int finish, int time)> edges, int n, IList<int> shortestPaths, IList<bool> visited, int currentNode, int currentPath)
+		{
+			if (shortestPaths[currentNode] > currentPath)
+			{
+				shortestPaths[currentNode] = currentPath;
+			}
+
+			if (visited.All(isVisited => isVisited))
+			{
+				return;
+			}
+
+			var nextMoves = edges
+				.Where(edge => edge.start == currentNode && !visited[edge.finish])
+				.ToList();
+
+			var newVisited = visited
+				.Select(v => v)
+				.ToList();
+
+			newVisited[currentNode] = true;
+
+			foreach (var (_, finish, time) in nextMoves)
+			{
+				GetShortestPaths(edges, n, shortestPaths, newVisited, finish, time + currentPath);
+			}
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/260-279/270/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem270
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestGetMessagePropagationTime_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var edges = new List<(int start, int finish, int time)>
+			{
+				(0, 1, 5),
+				(0, 2, 3),
+				(0, 5, 4),
+				(1, 3, 8),
+				(2, 3, 1),
+				(3, 5, 10),
+				(3, 4, 5)
+			};
+			var n = 5;
+
+			var expectedResult = 9;
+
+			// Act
+			var actualResult = Solution.GetMessagePropagationTime(edges, n);
+
+			// Assert
+			actualResult.Should().Be(expectedResult);
+		}
+	}
+}
