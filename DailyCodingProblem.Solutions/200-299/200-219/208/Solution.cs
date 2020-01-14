1d6ef4cda4b548102c72958d07b64991cc4ea168
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-14 16:20:22 +0200

    Solve #208

---
+++ DailyCodingProblem.Solutions/200-299/200-219/208/Solution.cs
@@ -0,0 +1,61 @@
+﻿using DailyCodingProblem.Solutions.Common;
+
+namespace DailyCodingProblem.Solutions.Problem208
+{
+	public class Solution
+	{
+		public static LinkedList<int> PartitionList(LinkedList<int> list, int k)
+		{
+			var root = (LinkedList<int>.LinkedListNode)null;
+			var left = (LinkedList<int>.LinkedListNode)null;
+
+			var rightRoot = (LinkedList<int>.LinkedListNode)null;
+			var right = (LinkedList<int>.LinkedListNode)null;
+
+			var current = list.Root;
+			while (current != null)
+			{
+				if (current.Value < k)
+				{
+					if (root == null)
+					{
+						root = new LinkedList<int>.LinkedListNode(current.Value);
+						left = root;
+					}
+					else
+					{
+						left = new LinkedList<int>.LinkedListNode(current.Value, left);
+					}
+				}
+				else
+				{
+					if (rightRoot == null)
+					{
+						rightRoot = new LinkedList<int>.LinkedListNode(current.Value);
+						right = rightRoot;
+					}
+					else
+					{
+						right = new LinkedList<int>.LinkedListNode(current.Value, right);
+					}
+				}
+
+				current = current.Next;
+			}
+
+			if (left != null)
+			{
+				if (right != null)
+				{
+					left.Next = rightRoot;
+				}
+			}
+			else if (right != null)
+			{
+				root = rightRoot;
+			}
+
+			return new LinkedList<int> { Root = root };
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/200-219/208/Tests.cs
@@ -0,0 +1,37 @@
+﻿using DailyCodingProblem.Solutions.Common;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem208
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestPartitionList_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var list = new LinkedList<int>();
+			list.Add(5);
+			list.Add(1);
+			list.Add(8);
+			list.Add(0);
+			list.Add(3);
+
+			var k = 3;
+
+			var expectedResult = new LinkedList<int>();
+			expectedResult.Add(1);
+			expectedResult.Add(0);
+			expectedResult.Add(5);
+			expectedResult.Add(8);
+			expectedResult.Add(3);
+
+			// Act
+			var actualResult = Solution.PartitionList(list, k);
+
+			// Assert
+			actualResult.Should().BeEquivalentTo(expectedResult);
+		}
+	}
+}
