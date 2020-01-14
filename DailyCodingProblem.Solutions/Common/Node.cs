65af01d917f101178e33202cacaf108f4103b28e
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-14 10:45:54 +0200

    Add parent to binary node

--- DailyCodingProblem.Solutions/Common/Node.cs
+++ DailyCodingProblem.Solutions/Common/Node.cs
@@ -1,23 +1,31 @@
 ﻿namespace DailyCodingProblem.Solutions.Common
 {
-		public class Node<T>
+	public class Node<T>
+	{
+		public Node(T value)
 		{
-			public Node(T value)
-			{
-				Value = value;
-			}
-
-			public Node(T value, Node<T> left, Node<T> right)
-				: this(value)
-			{
-				this.Left = left;
-				this.Right = right;
-			}
-
-			public Node<T> Right { get; set; }
-
-			public Node<T> Left { get; set; }
-
-			public T Value { get; set; }
+			Value = value;
 		}
+
+		public Node(T value, Node<T> left, Node<T> right)
+			: this(value)
+		{
+			this.Left = left;
+			this.Right = right;
+		}
+
+		public Node(T value, Node<T> left = null, Node<T> right = null, Node<T> parent = null)
+			: this(value,left,right)
+		{
+			this.Parent = parent;
+		}
+
+		public Node<T> Right { get; set; }
+
+		public Node<T> Left { get; set; }
+
+		public Node<T> Parent { get; set; }
+
+		public T Value { get; set; }
+	}
 }
