c800583479b3d997cd3ccb6df27bf738170c3a61
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-16 14:24:40 +0200

    Solve problem 133

---
+++ DailyCodingProblem.Solutions/100-199/120-139/Problem133/Solution.cs
@@ -0,0 +1,9 @@
+﻿using DailyCodingProblem.Solutions.Common;
+
+namespace DailyCodingProblem.Solutions.Problem133
+{
+	public class Solution
+	{
+		public static Node<int> GetInorderSuccessor(Node<int> node) => node.Right ?? node.Parent;
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/120-139/Problem133/Tests.cs
@@ -0,0 +1,32 @@
+﻿using DailyCodingProblem.Solutions.Common;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem133
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestGetInorderSuccessor_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var root = new Node<int>(10);
+			root.Left = new Node<int>(5, parent: root);
+			root.Right = new Node<int>(30, parent: root);
+
+			root.Right.Left = new Node<int>(22, parent: root.Right);
+			root.Right.Right = new Node<int>(35, parent: root.Right);
+
+			var inputNode = root.Right.Left;
+
+			var expectedResult = root.Right;
+
+			// Act
+			var actualResult = Solution.GetInorderSuccessor(inputNode);
+
+			// Assert
+			actualResult.Should().Be(expectedResult);
+		}
+	}
+}
