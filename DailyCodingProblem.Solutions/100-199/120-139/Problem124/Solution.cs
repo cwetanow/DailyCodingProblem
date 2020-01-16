9d643b1e8be8323cb5c5a5c97cad2bba4e1e5e3f
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-16 14:32:56 +0200

    Solve #124

---
+++ DailyCodingProblem.Solutions/100-199/120-139/Problem124/Solution.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DailyCodingProblem.Solutions.Problem124
+{
+	public class Solution
+	{
+		// Because the coins are fair, we assume 1/2 probability of tails. So after first round, 1/2 coins are left, after second is 1/2*1/2, etc...
+		public static int GetNumberOfRoundsUntillOneCoinLeft(int n) => (int)Math.Floor(Math.Log(n, 2)) + 1; // 2/2=1, log returns to 2
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/120-139/Problem124/Tests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem124
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase(50, 6)] // 50->25->13->7->4->2->1
+		[TestCase(51, 6)] // 51->26 (left)->13->7->4->2->1
+		public void TestGetNumberOfRoundsUntillOneCoinLeft_ShouldReturnCorrectly(int n, int expectedResult)
+		{
+			// Arrange, Act
+			var actualResult = Solution.GetNumberOfRoundsUntillOneCoinLeft(n);
+
+			// Assert
+			actualResult.Should().Be(expectedResult);
+		}
+	}
+}
