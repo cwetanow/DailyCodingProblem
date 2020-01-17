5f36cf1feb7d61b341c58e8d82d93d4017b72fda
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-17 16:18:25 +0200

    Solve problem 143

---
+++ DailyCodingProblem.Solutions/100-199/140-159/Problem143/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem143
+{
+	public class Solution
+	{
+		public static IEnumerable<int> PartitionList(List<int> input, int x)
+		{
+			var smaller = new LinkedList<int>();
+			var bigger = new LinkedList<int>();
+			var same = new LinkedList<int>();
+
+			foreach (var element in input)
+			{
+				if (element < x)
+				{
+					smaller.AddLast(element);
+				}
+				else if (element > x)
+				{
+					bigger.AddLast(element);
+				}
+				else
+				{
+					same.AddLast(element);
+				}
+			}
+
+			foreach (var i in bigger)
+			{
+				same.AddLast(i);
+			}
+
+			foreach (var i in same)
+			{
+				smaller.AddLast(i);
+			}
+
+			return smaller.ToList();
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/140-159/Problem143/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem143
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestPartitionList_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new List<int> { 9, 12, 3, 5, 14, 10, 10 };
+			var x = 10;
+
+			var expectedResult = new List<int> { 9, 3, 5, 10, 10, 12, 14 };
+
+			// Act
+			var actualResult = Solution.PartitionList(input, x);
+
+			// Assert
+			actualResult.Should().ContainInOrder(expectedResult);
+		}
+	}
+}
