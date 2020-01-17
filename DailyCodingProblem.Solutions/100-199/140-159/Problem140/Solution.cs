9a3f5481c14bc6aa01f9086d6c66be9e4ef90bfa
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-17 16:10:08 +0200

    Solve problem 140

---
+++ DailyCodingProblem.Solutions/100-199/140-159/Problem140/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace DailyCodingProblem.Solutions.Problem140
+{
+	public class Solution
+	{
+		public static IEnumerable<int> FindElementsThatAppearOnce(List<int> list)
+		{
+			var result = new HashSet<int>();
+
+			foreach (var element in list)
+			{
+				if (result.Contains(element))
+				{
+					result.Remove(element);
+				}
+				else
+				{
+					result.Add(element);
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/140-159/Problem140/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem140
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestFindElementsThatAppearOnce_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new List<int> { 2, 4, 6, 8, 10, 2, 6, 10 };
+			var expectedResult = new List<int> { 4, 8 };
+
+			// Act
+			var actualResult = Solution.FindElementsThatAppearOnce(input);
+
+			// Assert
+			actualResult.Should().BeEquivalentTo(expectedResult);
+		}
+	}
+}
