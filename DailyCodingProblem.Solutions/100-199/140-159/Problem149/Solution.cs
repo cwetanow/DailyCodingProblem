e54955183577df770a7d1b8f31e2abbe3c8785f4
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-17 15:55:37 +0200

    Solve problem 149

---
+++ DailyCodingProblem.Solutions/100-199/140-159/Problem149/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem149
+{
+	public class Solution
+	{
+		private readonly List<int> list;
+
+		public Solution(List<int> list)
+		{
+			this.list = list;
+		}
+
+		public int GetSublistSum(int start, int end) => list
+				.Where((_, index) => index >= start && index < end)
+				.Sum();
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/140-159/Problem149/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem149
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestGetSublistSum_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var list = new List<int> { 1, 2, 3, 4, 5 };
+			var start = 1;
+			var end = 3;
+
+			var expectedResult = 5;
+
+			var sut = new Solution(list);
+
+			// Act
+			var actualResult = sut.GetSublistSum(start, end);
+
+			// Assert
+			actualResult.Should().Be(expectedResult);
+		}
+	}
+}
