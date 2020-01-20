347adf4e93c9ecc7485f9779ebc68909a060dc67
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-20 16:13:23 +0200

    Solve problem 339

---
+++ DailyCodingProblem.Solutions/300-399/320-339/339/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace DailyCodingProblem.Solutions.Problem339
+{
+	public class Solution
+	{
+		public static bool AreThereThreeEntriesWhichAddUpToK(int k, int[] numbers)
+		{
+			return AreThereThreeEntriesWhichAddUpToK(k, numbers, 0, 0, 0);
+		}
+
+		private static bool AreThereThreeEntriesWhichAddUpToK(int k, int[] numbers, int currentIndex, int currentSum,
+			int summedNumbers)
+		{
+			if (currentIndex == numbers.Length || currentSum > k || summedNumbers > 3)
+			{
+				return false;
+			}
+
+			if (summedNumbers == 2 && (currentSum + numbers[currentIndex] == k))
+			{
+				return true;
+			}
+
+			return AreThereThreeEntriesWhichAddUpToK(k, numbers, currentIndex + 1, currentSum, summedNumbers) ||
+				   AreThereThreeEntriesWhichAddUpToK(k, numbers, currentIndex + 1, currentSum + numbers[currentIndex],
+					   summedNumbers + 1);
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/320-339/339/Tests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem339
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestAreThereThreeEntriesWhichAddUpToK_ShouldReturnTrue()
+		{
+			// Arrange
+			var numbers = new[] { 20, 303, 3, 4, 25 };
+			var k = 49;
+
+			// Act
+			var actualResult = Solution.AreThereThreeEntriesWhichAddUpToK(k, numbers);
+
+			// Assert
+			actualResult.Should().BeTrue();
+		}
+	}
+}
