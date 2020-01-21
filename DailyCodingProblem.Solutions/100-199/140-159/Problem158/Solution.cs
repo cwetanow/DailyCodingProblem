2d27b2f32be1e7d78b87ad90ab8f3ef85f603cb0
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-21 09:43:45 +0200

    Solve problem 158

---
+++ DailyCodingProblem.Solutions/100-199/140-159/Problem158/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem158
+{
+	public class Solution
+	{
+		public static int GetWaysToReachBottomRight(int[][] matrix, int rows, int cols)
+		{
+			var ways = Enumerable.Range(0, rows)
+				.Select(_ => Enumerable.Range(0, cols).Select(__ => 0).ToList())
+				.ToList();
+
+			Traverse(matrix, rows, cols, 0, 0, ways);
+
+			return ways[rows - 1][cols - 1];
+		}
+
+		private static void Traverse(int[][] matrix, int rows, int cols, int currentRow, int currentCol, List<List<int>> ways)
+		{
+			if (currentRow == rows || currentCol == cols)
+			{
+				return;
+			}
+
+			if (matrix[currentRow][currentCol] == 1)
+			{
+				return;
+			}
+
+			ways[currentRow][currentCol]++;
+
+			Traverse(matrix, rows, cols, currentRow + 1, currentCol, ways);
+			Traverse(matrix, rows, cols, currentRow, currentCol + 1, ways);
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/140-159/Problem158/Tests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem158
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestGetWaysToReachBottomRight_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new[]
+			{
+				new[] {0, 0, 1},
+				new[] {0, 0, 1},
+				new[] {1, 0, 0}
+			};
+
+			var rows = 3;
+			var cols = 3;
+
+			var expectedResult = 2;
+
+			// Act
+			var actualResult = Solution.GetWaysToReachBottomRight(input, rows, cols);
+
+			// Assert
+			actualResult.Should().Be(expectedResult);
+		}
+	}
+}
