b5fe8b09d57af9bffb1713f2ffb9cbe247c3bbed
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-21 10:09:29 +0200

    Solve problem 167

---
+++ DailyCodingProblem.Solutions/100-199/160-179/Problem167/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem167
+{
+	public class Solution
+	{
+		public static IEnumerable<(int first, int second)> GetPairsOfUniqueIndicesWithConcatenationOfWordsIsPalindrome(IList<string> words)
+		{
+			var result = new List<(int first, int second)>();
+			var visited = Enumerable.Range(0, words.Count)
+				.Select(_ => Enumerable.Range(0, words.Count).Select(__ => false).ToArray())
+				.ToArray();
+
+			for (var i = 0; i < words.Count; i++)
+			{
+				for (var j = 0; j < words.Count; j++)
+				{
+					if (i == j || visited[i][j])
+					{
+						continue;
+					}
+
+					if (IsPalindrome(words[i] + words[j]))
+					{
+						result.Add((i, j));
+					}
+
+					if (IsPalindrome(words[j] + words[i]))
+					{
+						result.Add((j, i));
+					}
+
+					visited[i][j] = true;
+					visited[j][i] = true;
+				}
+			}
+			return result;
+		}
+
+		private static bool IsPalindrome(string input)
+		{
+			var middle = input.Length / 2;
+
+			for (var i = 0; i <= middle; i++)
+			{
+				if (input[i] != input[input.Length - i - 1])
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/160-179/Problem167/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem167
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestGetPairsOfUniqueIndicesWithConcatenationOfWordsIsPalindrome_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new List<string> { "code", "edoc", "da", "d" };
+			var expectedResult = new List<(int first, int second)> { (0, 1), (1, 0), (2, 3) };
+
+			// Act
+			var actualResult = Solution.GetPairsOfUniqueIndicesWithConcatenationOfWordsIsPalindrome(input);
+
+			// Assert
+			actualResult.Should().BeEquivalentTo(expectedResult);
+		}
+	}
+}
