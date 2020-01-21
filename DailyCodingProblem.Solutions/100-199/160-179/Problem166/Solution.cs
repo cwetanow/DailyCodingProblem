84d70882db4ea082bd1d212ed7325c5049b53dfa
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-21 09:57:31 +0200

    Solve problem 166

---
+++ DailyCodingProblem.Solutions/100-199/160-179/Problem166/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DailyCodingProblem.Solutions.Problem166
+{
+	public class TwoDIterator : IEnumerable<int>
+	{
+		private readonly int[][] arrays;
+
+		public TwoDIterator(int[][] arrays)
+		{
+			this.arrays = arrays;
+		}
+
+		public IEnumerator<int> GetEnumerator()
+		{
+			foreach (var array in arrays)
+			{
+				foreach (var element in array)
+				{
+					yield return element;
+				}
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/160-179/Problem166/Tests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem166
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void Test_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new[]
+			{
+				new int[] {1, 2},
+				new int[] {3},
+				new int[] { },
+				new int[] {4, 5, 6},
+			};
+
+			var iterator = new TwoDIterator(input);
+
+			var expectedResult = new List<int> {1, 2, 3, 4, 5, 6};
+
+			// Act
+			var actualResult = iterator.ToList();
+
+			// Assert
+			actualResult.Should().BeEquivalentTo(expectedResult);
+		}
+	}
+}
