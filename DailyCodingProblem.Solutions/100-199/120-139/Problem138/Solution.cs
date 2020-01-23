712df7362a37aceedadcbb335f32403e219d334e
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-23 15:12:19 +0200

    Solve problem 138

---
+++ DailyCodingProblem.Solutions/100-199/120-139/Problem138/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem138
+{
+	public class Solution
+	{
+		private static IEnumerable<int> denominations = new List<int> { 25, 10, 5, 1 };
+
+		public static IEnumerable<int> FindMinimumNumberOfCoinsToSumN(int n)
+		{
+			var result = new List<int>();
+
+			var remaining = n;
+
+			while (remaining > 0)
+			{
+				var next = denominations
+					.Where(d => d <= remaining)
+					.Max();
+
+				result.Add(next);
+				remaining -= next;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/120-139/Problem138/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem138
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestFindMinimumNumberOfCoinsToSumN_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var n = 16;
+			var expectedResult = new List<int> {10, 5, 1};
+
+			// Act
+			var actualResult = Solution.FindMinimumNumberOfCoinsToSumN(n);
+
+			// Assert
+			actualResult.Should().BeEquivalentTo(expectedResult);
+		}
+	}
+}
