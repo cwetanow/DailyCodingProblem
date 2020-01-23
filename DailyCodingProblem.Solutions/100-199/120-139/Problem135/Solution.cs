8b16c00d832e7f493d6429b69a44870c0a73e174
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-23 15:47:38 +0200

    Solve #135

---
+++ DailyCodingProblem.Solutions/100-199/120-139/Problem135/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DailyCodingProblem.Solutions.Common;
+using Moq;
+
+namespace DailyCodingProblem.Solutions.Problem135
+{
+	public class Solution
+	{
+		public static int GetMinimumPathSumToLeaf(Node<int> root)
+		{
+			return GetMinimumPathSumToLeaf(root, 0);
+		}
+
+		private static int GetMinimumPathSumToLeaf(Node<int> node, int currentSum)
+		{
+			// Node is leaf
+			if (node == null)
+			{
+				return currentSum;
+			}
+
+			var leftMinSum = GetMinimumPathSumToLeaf(node.Left, currentSum + node.Value);
+			var rightMinSum = GetMinimumPathSumToLeaf(node.Right, currentSum + node.Value);
+
+			return Math.Min(leftMinSum, rightMinSum);
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/120-139/Problem135/Tests.cs
@@ -0,0 +1,32 @@
+﻿using DailyCodingProblem.Solutions.Common;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem135
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestGetMinimumPathSumToLeaf_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var root = new Node<int>(10,
+				new Node<int>(5,
+					right: new Node<int>(2)
+					),
+				new Node<int>(5,
+					right: new Node<int>(1,
+						left: new Node<int>(-1)
+						)));
+
+			var expectedResult = 15;
+
+			// Act
+			var actualResult = Solution.GetMinimumPathSumToLeaf(root);
+
+			// Assert
+			actualResult.Should().Be(expectedResult);
+		}
+	}
+}
