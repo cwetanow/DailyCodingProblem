48ef55aefca362092691f644c523687cb2718f9c
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-23 15:00:51 +0200

    Solve problem 181

---
+++ DailyCodingProblem.Solutions/100-199/180-199/Problem181/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace DailyCodingProblem.Solutions.Problem181
+{
+	public class Solution
+	{
+		public static IEnumerable<string> SplitToFewestPalindromes(string input)
+		{
+			var result = new List<string>();
+
+			var currentString = input;
+
+			var currentLength = currentString.Length;
+
+			while (!string.IsNullOrEmpty(currentString))
+			{
+				var current = currentString.Substring(0, currentLength);
+
+				if (IsPalindrome(current))
+				{
+					result.Add(current);
+					currentString = currentString.Remove(0, currentLength);
+					currentLength = currentString.Length;
+				}
+				else
+				{
+					currentLength--;
+				}
+			}
+
+			return result;
+		}
+
+		private static bool IsPalindrome(string input)
+		{
+			for (int i = 0; i <= input.Length / 2; i++)
+			{
+				if (input[i] != input[input.Length - i - 1])
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/100-199/180-199/Problem181/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem181
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase("racecarannakayak", new[] { "racecar", "anna", "kayak" })]
+		[TestCase("abc", new[] { "a", "b", "c" })]
+		public void TestSplitToFewestPalindromes_ShouldReturnCorrectly(string input, IEnumerable<string> expectedResult)
+		{
+			// Arrange, Act
+			var actualResult = Solution.SplitToFewestPalindromes(input);
+
+			// Assert
+			actualResult.Should().BeEquivalentTo(expectedResult);
+		}
+	}
+}
