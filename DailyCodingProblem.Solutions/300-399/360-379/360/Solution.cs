db66ae08db7f7bfbc8da48d094f0e0599a1a672f
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-24 10:49:06 +0200

    Solve #360

---
+++ DailyCodingProblem.Solutions/300-399/360-379/360/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem360
+{
+	public class Solution
+	{
+		public static IEnumerable<int> GetPlaylist(List<List<int>> ranks)
+		{
+			var result = new List<int>();
+
+			while (ranks.Any(ranking => ranking.Any()))
+			{
+				foreach (var rank in ranks)
+				{
+					if (rank.Any())
+					{
+						var next = rank.First();
+
+						var containingRanks = ranks.Where(r => r.Contains(next)).ToList();
+
+						if (containingRanks.Count(r => r.First() != next) == containingRanks.Count - 1 && containingRanks.Count > 1)
+						{
+							continue;
+						}
+
+						ranks
+							.Where(r => r.Contains(next))
+							.ToList()
+							.ForEach(r => r.Remove(next));
+
+						result.Add(next);
+
+						break;
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/360-379/360/Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem360
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestGetPlaylist_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new List<List<int>>
+			{
+				new List<int> {1, 7, 3 },
+				new List<int> { 2, 1, 6, 7, 9},
+				new List<int> { 3, 9, 5},
+			};
+
+			var expectedResult = new List<int>{ 2, 1, 6, 7, 3, 9, 5 };
+
+			// Act
+			var actualResult = Solution.GetPlaylist(input);
+
+			// Assert
+			actualResult.Should().BeEquivalentTo(expectedResult);
+		}
+	}
+}
