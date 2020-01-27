2e4a7a05a123de9b760263971861e8f601501945
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-27 10:36:10 +0200

    Solve 355

---
+++ DailyCodingProblem.Solutions/300-399/340-359/355/Solution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem355
+{
+	public class Solution
+	{
+		public static IEnumerable<int> RoundArrayToSmallestDifferenceWithEqualSums(IList<double> input)
+		{
+			return RoundArrayToSmallestDifferenceWithEqualSums(input, 0, (int)Math.Round(input.Sum()), 0, new List<int>(input.Count)).result;
+		}
+
+		private static (IEnumerable<int> result, double differencesSum) RoundArrayToSmallestDifferenceWithEqualSums(IList<double> input,
+			int currentIndex,
+			int inputSum,
+			double differencesSum,
+			IList<int> currentResult)
+		{
+			if (currentIndex == input.Count)
+			{
+				if (currentResult.Sum() != inputSum)
+				{
+					return (null, -1);
+				}
+
+				return (currentResult, differencesSum);
+			}
+
+			var roundedUpList = currentResult.Select(r => r).ToList();
+			roundedUpList.Add((int)Math.Ceiling(input[currentIndex]));
+
+			var (roundedUpResult, roundedUpDifferencesSum) = RoundArrayToSmallestDifferenceWithEqualSums(input, currentIndex + 1, inputSum,
+				differencesSum + Math.Abs(roundedUpList[currentIndex] - input[currentIndex]), roundedUpList);
+
+			var roundedDownList = currentResult.Select(r => r).ToList();
+			roundedDownList.Add((int)Math.Floor(input[currentIndex]));
+
+			var (roundedDownResult, roundedDownDifferencesSum) = RoundArrayToSmallestDifferenceWithEqualSums(input, currentIndex + 1, inputSum,
+				differencesSum + Math.Abs(roundedDownList[currentIndex] - input[currentIndex]), roundedDownList);
+
+			if (roundedUpResult != null && roundedDownResult != null)
+			{
+				return roundedUpDifferencesSum > roundedDownDifferencesSum
+					? (roundedDownResult, roundedDownDifferencesSum)
+					: (roundedUpResult, roundedUpDifferencesSum);
+			}
+
+			return roundedDownResult != null ?
+				(roundedDownResult, roundedDownDifferencesSum)
+				: (roundedUpResult, roundedUpDifferencesSum); 
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/340-359/355/Tests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem355
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestRoundArrayToSmallestDifferenceWithEqualSums_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new[] { 1.3, 2.3, 4.4 };
+			var expectedResult = new[] {1, 2, 5};
+
+			// Act
+			var actualResult = Solution.RoundArrayToSmallestDifferenceWithEqualSums(input);
+
+			// Assert
+			actualResult.Should().BeEquivalentTo(expectedResult);
+		}
+	}
+}
