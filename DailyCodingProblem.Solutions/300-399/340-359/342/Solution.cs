bedfc39189f896a675b8c0ecdcb93d7e553fc83e
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-27 15:49:26 +0200

    Solve problem 342

---
+++ DailyCodingProblem.Solutions/300-399/340-359/342/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem342
+{
+	public class Solution
+	{
+		public static T Reduce<T>(IList<T> list, Func<T, T, T> combiningFunc, T intialValue)
+		{
+			if (!list.Any())
+			{
+				return intialValue;
+			}
+
+			var result = combiningFunc.Invoke(intialValue, list.First());
+
+			for (var i = 1; i < list.Count; i++)
+			{
+				result = combiningFunc.Invoke(result, list[i]);
+			}
+
+			return result;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/340-359/342/Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem342
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestReduce_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = Enumerable.Range(1, 10).ToList();
+			var expectedResult = input.Sum();
+
+			var sumFunc = new Func<int, int, int>((a, b) => a + b);
+
+			// Act
+			var actualResult = Solution.Reduce(input, sumFunc, 0);
+
+			// Assert
+			actualResult.Should().Be(expectedResult);
+		}
+	}
+}
