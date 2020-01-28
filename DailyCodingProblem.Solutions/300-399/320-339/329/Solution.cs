c7f286c629d52901efe9f04f467b6aae7fa879fb
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-28 16:23:54 +0200

    Solve 329

---
+++ DailyCodingProblem.Solutions/300-399/320-339/329/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem329
+{
+	public class Solution
+	{
+		public static IEnumerable<(string guy, string girl)> PairPeople(int n,
+			ICollection<(string name, ICollection<string> preferences)> guyPreferences,
+			ICollection<(string name, ICollection<string> preferences)> girlPreferences)
+		{
+			var result = new List<(string guy, string girl)>();
+
+			while (guyPreferences.Any(p => p.preferences.Any()) && girlPreferences.Any(p => p.preferences.Any()))
+			{
+				DoWork(guyPreferences, girlPreferences, result, true);
+				DoWork(girlPreferences, guyPreferences, result, false);
+			}
+
+			return result;
+		}
+
+		private static void DoWork(ICollection<(string name, ICollection<string> preferences)> preferences,
+			ICollection<(string name, ICollection<string> preferences)> otherPreferencesCollection,
+			ICollection<(string guy, string girl)> result, bool guys)
+		{
+			var next = preferences
+				.GroupBy(p => p.preferences.FirstOrDefault())
+				.Where(g => g.Count() == 1)
+				.Select(g => g.First())
+				.ToList();
+
+			foreach (var preference in next)
+			{
+				var other = preference.preferences.First();
+
+				result.Add(guys ? (preference.name, other) : (other, preference.name));
+
+				preferences.Remove(preference);
+
+				var otherPreferences = otherPreferencesCollection.Single(p => p.name == other);
+				otherPreferencesCollection.Remove(otherPreferences);
+
+				foreach (var (_, preferencesCollection) in otherPreferencesCollection)
+				{
+					preferencesCollection.Remove(preference.name);
+				}
+
+				foreach (var (_, preferencesCollection) in preferences)
+				{
+					preferencesCollection.Remove(other);
+				}
+			}
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/320-339/329/Tests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem329
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestPairPeople_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var n = 3;
+			var guyPreferences = new List<(string guy, ICollection<string> preferences)>
+			{
+				("andrew", new List<string> {"caroline", "abigail", "betty" }),
+				("bill", new List<string> {"caroline", "betty", "abigail" }),
+				("chester", new List<string> {"betty", "caroline", "abigail" })
+			};
+
+			var girlPreferences = new List<(string girl, ICollection<string> preferences)>
+			{
+				("abigail", new List<string> {"andrew", "bill", "chester" }),
+				("betty", new List<string> { "bill", "andrew", "chester"}),
+				("caroline", new List<string> {"bill", "chester", "andrew" })
+			};
+
+			var expectedResult = new List<(string guy, string girl)> {
+				("andrew","abigail"),
+				("bill","caroline"),
+				("chester","betty")
+			};
+
+			// Act
+			var actualResult = Solution.PairPeople(n, guyPreferences, girlPreferences);
+
+			// Assert
+			actualResult.Should().BeEquivalentTo(expectedResult);
+		}
+	}
+}
