b5c57c79d84eddee84df46668d56071e935ae305
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-01-30 13:57:37 +0200

    Solve problem 274

---
+++ DailyCodingProblem.Solutions/200-299/260-279/274/Solution.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+
+namespace DailyCodingProblem.Solutions.Problem274
+{
+	public class Solution
+	{
+		public static int Eval(string input)
+		{
+			input = input.Replace(" ", string.Empty);
+			var reader = new StringReader(input);
+
+
+			return Eval(reader);
+		}
+
+		private static int Eval(StringReader reader)
+		{
+			var result = 0;
+			var currentSign = '+';
+
+			while (true)
+			{
+				var next = reader.Read();
+
+				if (next < 0)
+				{
+					return result;
+				}
+
+				var nextChar = (char)next;
+				if (nextChar == ')')
+				{
+					return result;
+				}
+
+				if (char.IsNumber(nextChar))
+				{
+					var number = int.Parse(nextChar.ToString());
+					if (currentSign == '-')
+					{
+						number *= -1;
+					}
+
+					result += number;
+				}
+				else if (nextChar == '(')
+				{
+					var number = Eval(reader);
+					if (currentSign == '-')
+					{
+						number *= -1;
+					}
+
+					result += number;
+				}
+				else
+				{
+					currentSign = nextChar;
+				}
+			}
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/260-279/274/Tests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem274
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase("-1 + (2 + 3)", 4)]
+		[TestCase("(-1 + (2 + 3)) + 5 - (3 + 7)", -1)]
+		public void TestEval_ShouldReturnCorrectly(string input, int expectedResult)
+		{
+			// Arrange, Act
+			var actualResult = Solution.Eval(input);
+
+			// Assert
+			actualResult.Should().Be(expectedResult);
+		}
+	}
+}
