05ffc04651ddf3f6f42424c7d2216c8c6adc3190
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-09 16:23:14 +0200

    Solve problem 321

---
+++ DailyCodingProblem.Solutions/300-399/320-339/321/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem321
+{
+	public class Solution
+	{
+		public static int FindSmallestNumberOfStepsToReachOne(int n) => FindSmallestNumberOfStepsToReachOne(n, 0);
+
+		private static int FindSmallestNumberOfStepsToReachOne(int n, int currentSteps)
+		{
+			if (n < 0)
+			{
+				return int.MaxValue;
+			}
+
+			if (n == 1)
+			{
+				return currentSteps;
+			}
+
+			var items = Enumerable.Range(1, n / 2)
+				.Where(number => n % number == 0)
+				.Select(number => Math.Max(n / number, number))
+				.Distinct()
+				.ToList();
+
+			items.Remove(n);
+			items.Add(n - 1);
+
+			return items
+				.Select(number => FindSmallestNumberOfStepsToReachOne(number, currentSteps + 1))
+				.Min();
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/320-339/321/Tests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem321
+{
+	[TestFixture]
+	public class Tests
+	{
+		[TestCase(100,5)]
+		public void TestFindSmallestNumberOfStepsToReachOne_ShouldReturnCorrectly(int n, int expectedResult)
+		{
+			// Arrange, Act
+			var actualResult = Solution.FindSmallestNumberOfStepsToReachOne(n);
+
+			// Assert
+			actualResult.Should().Be(expectedResult);
+		}
+	}
+}
