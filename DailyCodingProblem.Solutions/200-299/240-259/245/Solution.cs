b5f9cb93172946f24cc528cb1a09e918f48f36f3
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-03-10 11:09:35 +0200

    Solve 245

---
+++ DailyCodingProblem.Solutions/200-299/240-259/245/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem245
+{
+	public class Solution
+	{
+		public static int GetMinimumNumberOfJumpsToEnd(List<int> input) =>
+			GetMinimumNumberOfJumpsToEnd(input, 0, 0);
+
+		private static int GetMinimumNumberOfJumpsToEnd(List<int> input, int currentIndex, int currentJumps)
+		{
+			if (currentIndex >= input.Count)
+			{
+				return int.MaxValue;
+			}
+
+			if (currentIndex == input.Count - 1)
+			{
+				return currentJumps;
+			}
+
+			var minJumps = Enumerable.Range(1, input[currentIndex])
+				.Select(jump => GetMinimumNumberOfJumpsToEnd(input, currentIndex + jump, currentJumps + 1))
+				.ToList();
+
+			if (!minJumps.Any())
+			{
+				return int.MaxValue;
+			}
+			return minJumps.Min();
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/200-299/240-259/245/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem245
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void Test_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var input = new List<int> { 6, 2, 4, 0, 5, 1, 1, 4, 2, 9 };
+			var expectedResult = 2;
+
+			// Act
+			var actualResult = Solution.GetMinimumNumberOfJumpsToEnd(input);
+
+			// Assert
+			actualResult.Should().Be(expectedResult);
+		}
+	}
+}
