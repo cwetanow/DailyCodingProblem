6aa1ac9d50526ca810a8f59c32171495eba3b8db
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-05-12 14:11:47 +0300

    #314

---
+++ DailyCodingProblem.Solutions/300-399/300-319/314/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem314
+{
+	public class Solution
+	{
+		public static int GetMinimumBroadcastRange(IEnumerable<int> listeners, ICollection<int?> towers)
+		{
+			var minimumRange = int.MinValue;
+
+			foreach (var listener in listeners)
+			{
+				var previousTower = towers.LastOrDefault(t => t.HasValue && t.Value <= listener);
+				var nextTower = towers.FirstOrDefault(t => t.HasValue && t.Value > listener);
+
+				if (previousTower.HasValue && nextTower.HasValue)
+				{
+					var previousDistance = listener - previousTower.Value;
+					var nextDistance = nextTower.Value - listener;
+
+					minimumRange = Math.Max(minimumRange, Math.Min(previousDistance, nextDistance));
+				}
+				else if (previousTower.HasValue)
+				{
+					var previousDistance = listener - previousTower.Value;
+
+					minimumRange = Math.Max(minimumRange, previousDistance);
+				}
+				else if (nextTower.HasValue)
+				{
+					var nextDistance = nextTower.Value - listener;
+
+					minimumRange = Math.Max(minimumRange, nextDistance);
+				}
+			}
+
+			return minimumRange;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/300-399/300-319/314/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem314
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestGetMinimumBroadcastRange_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var listeners= new List<int> { 1, 5, 11, 20 };
+			var towers= new List<int?> { 4, 8, 15 };
+			var expectedMinimumRange = 5;
+
+			// Act
+			var actualMinimumRange = Solution.GetMinimumBroadcastRange(listeners, towers);
+
+			// Assert
+			actualMinimumRange.Should().Be(expectedMinimumRange);
+		}
+	}
+}
