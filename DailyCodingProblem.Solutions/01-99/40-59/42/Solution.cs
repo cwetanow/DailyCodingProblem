af11ffee4986e50e11a047bff35467d8c88f72b4
Ivan Mladenov <mladenovv.ivan@gmail.com>
2020-05-13 11:55:07 +0300

    Solve 42

---
+++ DailyCodingProblem.Solutions/01-99/40-59/42/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyCodingProblem.Solutions.Problem42
+{
+	public class Solution
+	{
+		public static IEnumerable<int> GetSubsetThatAddsToK(IList<int> numbers, int k)
+		{
+			return GetSubsetThatAddsToK(numbers, k, 0, 0, new List<int>());
+		}
+
+		private static IEnumerable<int> GetSubsetThatAddsToK(IList<int> numbers, int k, int currentIndex, int currentSum, IEnumerable<int> currentNumbers)
+		{
+			if (currentSum == k)
+			{
+				return currentNumbers;
+			}
+
+			if (currentSum > k || currentIndex == numbers.Count())
+			{
+				return null;
+			}
+
+			var withNumbers = new List<int>(currentNumbers) {numbers[currentIndex]};
+			var with = GetSubsetThatAddsToK(numbers, k, currentIndex + 1, currentSum + numbers[currentIndex],
+				withNumbers);
+
+			var without = GetSubsetThatAddsToK(numbers, k, currentIndex + 1, currentSum, currentNumbers);
+
+			return with ?? without;
+		}
+	}
+}
---
+++ DailyCodingProblem.Solutions/01-99/40-59/42/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DailyCodingProblem.Solutions.Problem42
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void TestGetSubsetsThatAddsToK_ShouldReturnCorrectly()
+		{
+			// Arrange
+			var s = new List<int> { 12, 1, 61, 5, 9, 2 };
+			var k = 24;
+
+			var expected = new List<int> { 12, 9, 2, 1 };
+
+			// Act
+			var actual = Solution.GetSubsetThatAddsToK(s, k);
+
+			// Assert
+			actual.Should().BeEquivalentTo(expected);
+		}
+	}
+}
